b2f5daad885ed8bbc5a9854a5a0abd1adf1c5db6
Edgar Orendain <edgar@orendainx.com>
2021-12-27 08:19:58 -0800

    Added 'SkipIntro' mod, a mod for skipping the intro loading screen.

---
+++ SkipIntro/SkipIntroMod.cs
@@ -0,0 +1,13 @@
+﻿using MelonLoader;
+
+namespace SkipIntro
+{
+    internal class SkipIntroMod : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            var harmony = new HarmonyLib.Harmony("com.orendain.demeomods.skipintro");
+            SkipIntroPatcher.Patch(harmony);
+        }
+    }
+}
---
+++ SkipIntro/SkipIntroPatcher.cs
@@ -0,0 +1,24 @@
+﻿using Boardgame;
+using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+
+namespace SkipIntro
+{
+    internal static class SkipIntroPatcher
+    {
+        internal static void Patch(HarmonyLib.Harmony harmony)
+        {
+            harmony.Patch(
+                original: typeof(MotherbrainSceneUtil).GetMethod("LoadIntro"),
+                prefix: new HarmonyMethod(typeof(SkipIntroPatcher), nameof(MotherbrainSceneUtil_LoadIntro_Prefix)));
+        }
+
+        private static bool MotherbrainSceneUtil_LoadIntro_Prefix(ref AsyncOperation __result)
+        {
+            MelonLogger.Msg("Skipping the intro scene.");
+            __result = MotherbrainSceneUtil.LoadLobby();
+            return false;
+        }
+    }
+}
