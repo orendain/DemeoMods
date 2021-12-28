3a1cbc53ac2355a2badbc520af72a66e92fa010b
Restyled.io <commits@restyled.io>
2021-12-28 15:22:16 +0000

    Restyled by clang-format

--- RoomFinder/UI/RoomListPanel.cs
+++ RoomFinder/UI/RoomListPanel.cs
@@ -8,111 +8,111 @@ using MelonLoader;
 using Photon.Realtime;
 using UnityEngine;
 
-namespace RoomFinder.UI
-{
-internal class RoomListPanel
-{
+namespace RoomFinder.UI {
+  internal class RoomListPanel {
     private readonly GameObject panelObject;
 
-    public static RoomListPanel NewInstance()
-    {
-        return new RoomListPanel();
+    public static RoomListPanel NewInstance() { return new RoomListPanel(); }
+
+    private RoomListPanel() { panelObject = new GameObject("RoomListPanel"); }
+
+    public GameObject Reinitialize(List<RoomInfo> rooms) {
+      foreach (Transform child in panelObject.transform) {
+        UnityEngine.Object.Destroy(child.gameObject);
+      }
+
+      RenderHeader();
+      for (var i = 0; i < rooms.Count; i++) {
+        RenderRoomRow(rooms[i], i);
+      }
+
+      return panelObject;
     }
 
-    private RoomListPanel()
-    {
-        panelObject = new GameObject("RoomListPanel");
+    private void RenderHeader() {
+      var headerContainer = new GameObject("Header");
+      headerContainer.transform.SetParent(panelObject.transform,
+                                          worldPositionStays: false);
+
+      var joinLabel = UiUtil.CreateLabelText("Code");
+      joinLabel.transform.SetParent(headerContainer.transform,
+                                    worldPositionStays: false);
+      joinLabel.transform.localPosition = new Vector3(-3f, 0, 0);
+
+      var gameLabel = UiUtil.CreateLabelText("Game");
+      gameLabel.transform.SetParent(headerContainer.transform,
+                                    worldPositionStays: false);
+      gameLabel.transform.localPosition = new Vector3(-0.4f, 0, 0);
+
+      var floorLabel = UiUtil.CreateLabelText("Floor");
+      floorLabel.transform.SetParent(headerContainer.transform,
+                                     worldPositionStays: false);
+      floorLabel.transform.localPosition = new Vector3(1.75f, 0, 0);
+
+      var playersLabel = UiUtil.CreateLabelText("Players");
+      playersLabel.transform.SetParent(headerContainer.transform,
+                                       worldPositionStays: false);
+      playersLabel.transform.localPosition = new Vector3(3.5f, 0, 0);
     }
 
-    public GameObject Reinitialize(List<RoomInfo> rooms)
-    {
-        foreach (Transform child in panelObject.transform)
-        {
-            UnityEngine.Object.Destroy(child.gameObject);
-        }
+    private void RenderRoomRow(RoomInfo room, int row) {
+      var yOffset = (1 + row) * -1f;
+      var roomRowContainer = new GameObject($"Row{row}");
+      roomRowContainer.transform.SetParent(panelObject.transform,
+                                           worldPositionStays: false);
+      roomRowContainer.transform.localPosition = new Vector3(0, yOffset, 0);
 
-        RenderHeader();
-        for (var i = 0; i < rooms.Count; i++)
-        {
-            RenderRoomRow(rooms[i], i);
-        }
+      object obj;
+      var gameType = room.CustomProperties.TryGetValue("at", out obj)
+                         ? (LevelSequence.GameType)obj
+                         : LevelSequence.GameType.Invalid;
+      var floorIndex =
+          room.CustomProperties.TryGetValue("fi", out obj) ? (int)obj : -1;
 
-        return panelObject;
+      if (gameType == LevelSequence.GameType.Invalid || floorIndex < 0)
+        return;
+
+      var joinButton = UiUtil.CreateButton(JoinRoomAction(room.Name));
+      joinButton.transform.SetParent(roomRowContainer.transform,
+                                     worldPositionStays: false);
+      joinButton.transform.localScale = new Vector3(0.4f, 0.7f, 0.7f);
+      joinButton.transform.localPosition = new Vector3(-3f, 0, 0);
+
+      var joinText = UiUtil.CreateText(room.Name, Color.white,
+                                       UiUtil.DefaultLabelFontSize);
+      joinText.transform.SetParent(roomRowContainer.transform,
+                                   worldPositionStays: false);
+      joinText.transform.localPosition = new Vector3(-3f, 0, 0);
+
+      var gameName = StringifyGameType(gameType);
+      var gameLabel = UiUtil.CreateLabelText(gameName);
+      gameLabel.transform.SetParent(roomRowContainer.transform,
+                                    worldPositionStays: false);
+      gameLabel.transform.localPosition = new Vector3(-0.4f, 0, 0);
+
+      var floorLabel = UiUtil.CreateLabelText(floorIndex.ToString());
+      floorLabel.transform.SetParent(roomRowContainer.transform,
+                                     worldPositionStays: false);
+      floorLabel.transform.localPosition = new Vector3(1.75f, 0, 0);
+
+      var playersText = $"{room.PlayerCount}/{room.MaxPlayers}";
+      var playersLabel = UiUtil.CreateLabelText(playersText);
+      playersLabel.transform.SetParent(roomRowContainer.transform,
+                                       worldPositionStays: false);
+      playersLabel.transform.localPosition = new Vector3(3.25f, 0, 0);
     }
 
-    private void RenderHeader()
-    {
-        var headerContainer = new GameObject("Header");
-        headerContainer.transform.SetParent(panelObject.transform, worldPositionStays: false);
-
-        var joinLabel = UiUtil.CreateLabelText("Code");
-        joinLabel.transform.SetParent(headerContainer.transform, worldPositionStays: false);
-        joinLabel.transform.localPosition = new Vector3(-3f, 0, 0);
-
-        var gameLabel = UiUtil.CreateLabelText("Game");
-        gameLabel.transform.SetParent(headerContainer.transform, worldPositionStays: false);
-        gameLabel.transform.localPosition = new Vector3(-0.4f, 0, 0);
-
-        var floorLabel = UiUtil.CreateLabelText("Floor");
-        floorLabel.transform.SetParent(headerContainer.transform, worldPositionStays: false);
-        floorLabel.transform.localPosition = new Vector3(1.75f, 0, 0);
-
-        var playersLabel = UiUtil.CreateLabelText("Players");
-        playersLabel.transform.SetParent(headerContainer.transform, worldPositionStays: false);
-        playersLabel.transform.localPosition = new Vector3(3.5f, 0, 0);
+    private static string StringifyGameType(LevelSequence.GameType gameType) {
+      return gameType.ToString();
     }
 
-    private void RenderRoomRow(RoomInfo room, int row)
-    {
-        var yOffset = (1 + row) * -1f;
-        var roomRowContainer = new GameObject($"Row{row}");
-        roomRowContainer.transform.SetParent(panelObject.transform, worldPositionStays: false);
-        roomRowContainer.transform.localPosition = new Vector3(0, yOffset, 0);
-
-        object obj;
-        var gameType = room.CustomProperties.TryGetValue("at", out obj)
-                       ? (LevelSequence.GameType) obj
-                       : LevelSequence.GameType.Invalid;
-        var floorIndex = room.CustomProperties.TryGetValue("fi", out obj) ? (int) obj : -1;
-
-        if (gameType == LevelSequence.GameType.Invalid || floorIndex < 0) return;
-
-        var joinButton = UiUtil.CreateButton(JoinRoomAction(room.Name));
-        joinButton.transform.SetParent(roomRowContainer.transform, worldPositionStays: false);
-        joinButton.transform.localScale = new Vector3(0.4f, 0.7f, 0.7f);
-        joinButton.transform.localPosition = new Vector3(-3f, 0, 0);
-
-        var joinText = UiUtil.CreateText(room.Name, Color.white, UiUtil.DefaultLabelFontSize);
-        joinText.transform.SetParent(roomRowContainer.transform, worldPositionStays: false);
-        joinText.transform.localPosition = new Vector3(-3f, 0, 0);
-
-        var gameName = StringifyGameType(gameType);
-        var gameLabel = UiUtil.CreateLabelText(gameName);
-        gameLabel.transform.SetParent(roomRowContainer.transform, worldPositionStays: false);
-        gameLabel.transform.localPosition = new Vector3(-0.4f, 0, 0);
-
-        var floorLabel = UiUtil.CreateLabelText(floorIndex.ToString());
-        floorLabel.transform.SetParent(roomRowContainer.transform, worldPositionStays: false);
-        floorLabel.transform.localPosition = new Vector3(1.75f, 0, 0);
-
-        var playersText = $"{room.PlayerCount}/{room.MaxPlayers}";
-        var playersLabel = UiUtil.CreateLabelText(playersText);
-        playersLabel.transform.SetParent(roomRowContainer.transform, worldPositionStays: false);
-        playersLabel.transform.localPosition = new Vector3(3.25f, 0, 0);
+    private static Action JoinRoomAction(string roomCode) {
+      return () => {
+        MelonLogger.Msg($"Joining room [{roomCode}].");
+        Traverse.Create(GameContextState.LobbyMenuController)
+            .Method("JoinGame", roomCode, true)
+            .GetValue();
+      };
     }
-
-    private static string StringifyGameType(LevelSequence.GameType gameType)
-    {
-        return gameType.ToString();
-    }
-
-    private static Action JoinRoomAction(string roomCode)
-    {
-        return () =>
-        {
-            MelonLogger.Msg($"Joining room [{roomCode}].");
-            Traverse.Create(GameContextState.LobbyMenuController).Method("JoinGame", roomCode, true).GetValue();
-        };
-    }
-}
+  }
 }
