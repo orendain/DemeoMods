5fadd0cccd22d0ddb6705a3d1c330b411b601ab2
Edgar Orendain <edgar@orendainx.com>
2021-12-29 01:01:00 -0800

    Bump version to 1.1.0.

--- SkipIntro/Properties/AssemblyInfo.cs
+++ SkipIntro/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 using MelonLoader;
 using SkipIntro;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SkipIntro")]
@@ -15,8 +15,8 @@ using SkipIntro;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -26,16 +26,16 @@ using SkipIntro;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Melon Loader.
-[assembly: MelonInfo(typeof(SkipIntroMod), "SkipIntro", "1.0.1", "Orendain", "https://github.com/orendain/DemeoMods")]
+[assembly: MelonInfo(typeof(SkipIntroMod), "SkipIntro", "1.1.0", "Orendain", "https://github.com/orendain/DemeoMods")]
 [assembly: MelonGame("Resolution Games", "Demeo")]
