51b0239aeac44d406738f762add930365f6e43e9
Edgar Orendain <edgar@orendainx.com>
2022-01-08 15:30:02 -0800

    wip dump

--- Common/UI/PageStack.cs
+++ Common/UI/PageStack.cs
@@ -9,23 +9,58 @@ internal class PageStack
 {
 
     private GameObject _gameObject;
+    private GameObject _background;
+
     private int currentPage;
     private List<GameObject> pages;
 
     private GameObject _stackPanel;
-    private UiHelper uiHelper;
+    private UiHelper _uiHelper;
 
-    public static PageStack newInstance(UiHelper uiHelper)
+    public static PageStack newInstance(string menuTitle, UiHelper uiHelper)
     {
-        return new PageStack(uiHelper);
+        return new PageStack(menuTitle, uiHelper);
     }
 
-    private PageStack(UiHelper uiHelper)
+    private PageStack(string menuTitle, UiHelper uiHelper)
     {
         _gameObject = new GameObject("PageStack");
         pages = new List<GameObject>();
         currentPage = 0;
-        this.uiHelper = uiHelper;
+        this._uiHelper = uiHelper;
+    }
+
+    private void Initialize()
+    {
+        _gameObject.transform.SetParent(_uiHelper.DemeoResource.LobbyAnchor.transform, worldPositionStays: true);
+        _gameObject.transform.position = new Vector3(25, 30, 0);
+
+        _background = new GameObject("PageStackBackground");
+        _background.AddComponent<MeshFilter>().mesh = _uiHelper.DemeoResource.MenuBoxMesh;
+        _background.AddComponent<MeshRenderer>().material = _uiHelper.DemeoResource.MenuBoxMaterial;
+
+        _background.transform.SetParent(this.transform, worldPositionStays: false);
+        _background.transform.localPosition = new Vector3(0, -3.6f, 0);
+        _background.transform.localRotation =
+            Quaternion.Euler(-90, 0, 0); // Un-flip card from it's default face-up position.
+        _background.transform.localScale = new Vector3(2, 1, 2.5f);
+
+        var menuTitle = _uiHelper.CreateMenuHeaderText("Public Rooms");
+        menuTitle.transform.SetParent(this.transform, worldPositionStays: false);
+        menuTitle.transform.localPosition = new Vector3(0, 2.375f, 0);
+
+        var refreshButton = _uiHelper.CreateButton(RefreshRoomList);
+        refreshButton.transform.SetParent(this.transform, worldPositionStays: false);
+        refreshButton.transform.localPosition = new Vector3(0, 0.3f, 0);
+
+        var refreshText = _uiHelper.CreateButtonText("Refresh");
+        refreshText.transform.SetParent(this.transform, worldPositionStays: false);
+        refreshText.transform.localPosition = new Vector3(0, 0.3f, 0);
+
+        // TODO(orendain): Fix so that ray interacts with entire object.
+        this.gameObject.AddComponent<BoxCollider>();
+
+        _isInitialized = true;
     }
 
     /** Adds the specified page to the stack.
@@ -79,16 +114,16 @@ internal class PageStack
         nav.transform.SetParent(nav.transform, worldPositionStays: false);
         nav.transform.localPosition = new Vector3(0, 5, 0);
 
-        var prevButton = uiHelper.CreateButton(OnPreviousPageClick);
+        var prevButton = _uiHelper.CreateButton(OnPreviousPageClick);
         prevButton.transform.SetParent(nav.transform, worldPositionStays: false);
         prevButton.transform.localPosition = new Vector3(0, 0, 0);
 
         var pageStatusText = string.Format("%s/%s", currentPage, pages.Count);
-        var pageStatus = uiHelper.CreateLabelText(pageStatusText);
+        var pageStatus = _uiHelper.CreateLabelText(pageStatusText);
         pageStatus.transform.SetParent(nav.transform, worldPositionStays:false);
         pageStatus.transform.localPosition = new Vector3(1, 0, 0);
 
-        var nextButton = uiHelper.CreateButton(OnNextPageClick);
+        var nextButton = _uiHelper.CreateButton(OnNextPageClick);
         nextButton.transform.SetParent(nav.transform, worldPositionStays: false);
         nextButton.transform.localPosition = new Vector3(2, 0, 0);
     }
