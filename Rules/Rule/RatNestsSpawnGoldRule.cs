4c899d986fd9040e526a40c037649e5582f2d94b
Edgar Orendain <edgar@orendainx.com>
2022-01-25 00:55:44 -0800

    Add rule for making rat nests spawn gold. (#32)

---
+++ Rules/Rule/RatNestsSpawnGoldRule.cs
@@ -0,0 +1,37 @@
+﻿namespace Rules.Rule
+{
+    using System.Linq;
+    using Boardgame.BoardEntities.Abilities;
+    using DataKeys;
+    using UnityEngine;
+
+    public sealed class RatNestsSpawnGoldRule : RulesAPI.Rule
+    {
+        public override string Description => "Rat nests spawn gold";
+
+        private readonly int _maxPileCount;
+        private int _originalSpawnAmount;
+
+        public RatNestsSpawnGoldRule(int maxPileCount)
+        {
+            _maxPileCount = maxPileCount;
+        }
+
+        protected override void OnActivate()
+        {
+            var abilities = Resources.FindObjectsOfTypeAll<Ability>();
+            var ability = abilities.First(c => c.name.Equals("SpawnRat(Clone)"));
+            ability.pieceToSpawn = BoardPieceId.GoldPile;
+            _originalSpawnAmount = ability.spawnMaxAmount;
+            ability.spawnMaxAmount = _maxPileCount;
+        }
+
+        protected override void OnDeactivate()
+        {
+            var abilities = Resources.FindObjectsOfTypeAll<Ability>();
+            var ability = abilities.First(c => c.name.Equals("SpawnRat(Clone)"));
+            ability.pieceToSpawn = BoardPieceId.Rat;
+            ability.spawnMaxAmount = _originalSpawnAmount;
+        }
+    }
+}
