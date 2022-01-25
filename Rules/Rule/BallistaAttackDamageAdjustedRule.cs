cbc7331b7fdfe53e6891bbac82423900714bea81
Edgar Orendain <edgar@orendainx.com>
2022-01-25 00:52:18 -0800

    Add ballista damage and action points adjustment rules.

---
+++ Rules/Rule/BallistaAttackDamageAdjustedRule.cs
@@ -0,0 +1,34 @@
+﻿namespace Rules.Rule
+{
+    using System.Linq;
+    using Boardgame.BoardEntities.Abilities;
+    using UnityEngine;
+
+    public sealed class BallistaAttackDamageAdjustedRule : RulesAPI.Rule
+    {
+        public override string Description => "Ballista attack damage are adjusted";
+
+        private readonly int _attackDamage;
+        private int _originalAttackDamage;
+
+        public BallistaAttackDamageAdjustedRule(int attackDamage)
+        {
+            _attackDamage = attackDamage;
+        }
+
+        protected override void OnActivate()
+        {
+            var abilities = Resources.FindObjectsOfTypeAll<Ability>();
+            var ability = abilities.First(c => c.name.Equals("TurretDamageProjectile(Clone)"));
+            _originalAttackDamage = ability.abilityDamage.targetDamage;
+            ability.abilityDamage.targetDamage = _attackDamage;
+        }
+
+        protected override void OnDeactivate()
+        {
+            var abilities = Resources.FindObjectsOfTypeAll<Ability>();
+            var ability = abilities.First(c => c.name.Equals("TurretDamageProjectile(Clone)"));
+            ability.abilityDamage.targetDamage = _originalAttackDamage;
+        }
+    }
+}
