03269c254423a4374ea349eac801d514dce2eae9
Edgar Orendain <edgar@orendainx.com>
2022-01-25 00:14:11 -0800

    Define Rules mod skeleton.

---
+++ Rules/RulesMod.cs
@@ -0,0 +1,23 @@
+﻿namespace Rules
+{
+    using MelonLoader;
+
+    internal class RulesMod : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            RegisterNewRuleTypes();
+            RegisterNewRuleSets();
+        }
+
+        private static void RegisterNewRuleTypes()
+        {
+            var registrar = RulesAPI.Registrar.Instance();
+        }
+
+        private static void RegisterNewRuleSets()
+        {
+            var registrar = RulesAPI.Registrar.Instance();
+        }
+    }
+}
