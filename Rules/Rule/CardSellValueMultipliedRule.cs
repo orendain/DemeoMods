648a68b1beb5caa28314c34a159193f42e7a78d5
Edgar Orendain <edgar@orendainx.com>
2022-01-25 20:48:46 -0800

    Add rule which increases card sell values.

---
+++ Rules/Rule/CardSellValueMultipliedRule.cs
@@ -0,0 +1,40 @@
+﻿namespace Rules.Rule
+{
+    using System.Reflection;
+    using HarmonyLib;
+
+    public sealed class CardSellValueMultipliedRule : RulesAPI.Rule
+    {
+        public override string Description => "Card sell values are multiplied";
+
+        private static double _multiplier;
+        private static bool _isActivated;
+
+        public CardSellValueMultipliedRule(double multiplier)
+        {
+            _multiplier = multiplier;
+        }
+
+        protected override void OnActivate() => _isActivated = true;
+
+        protected override void OnDeactivate() => _isActivated = false;
+
+        private static void OnPatch(Harmony harmony)
+        {
+            harmony.Patch(
+                original: AccessTools.Inner(typeof(CardShopController), "CardShopInfoProvider").GetTypeInfo()
+                    .GetDeclaredMethod("GetCardSellValue"),
+                postfix: new HarmonyMethod(typeof(CardSellValueMultipliedRule), nameof(CardShopInfoProvider_GetCardSellValue_Postfix)));
+        }
+
+        private static void CardShopInfoProvider_GetCardSellValue_Postfix(ref int __result)
+        {
+            if (!_isActivated)
+            {
+                return;
+            }
+
+            __result = (int)(__result * _multiplier);
+        }
+    }
+}
