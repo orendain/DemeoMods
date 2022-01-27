22daa4fbef60aa8a785b11fe19956fd536be2c11
Edgar Orendain <edgar@orendainx.com>
2022-01-26 17:20:42 -0800

    Remove unneeded comments.

--- RulesAPI/Rule.cs
+++ RulesAPI/Rule.cs
@@ -19,45 +19,6 @@
             OnDeactivate();
         }
 
-        // Recommendation is using passive voice for rule names.
-
-        /// <summary>
-        /// Called when the game is patched with this rule.
-        /// </summary>
-        /// <remarks>
-        ///     <para>
-        ///         This method should perform any required patching using the given patcher.
-        ///     </para>
-        ///     <para>
-        ///         Rules should use a flag (e.g., <c>_isActivated</c>) in the patched method to ensure they make modifications only while the rule is activated.
-        ///     </para>
-        /// </remarks>
-        // private static void OnPatch(HarmonyLib.Harmony harmony);
-
-        /// <summary>
-        /// Called when a game is started.
-        /// </summary>
-        /// <remarks>
-        ///     This method is where one could make changes to, for example:
-        ///     <list type="bullet">
-        ///         <item>
-        ///             <description>A <c>PieceConfig</c>.</description>
-        ///         </item>
-        ///         <item>
-        ///             <description>An ability's <c>(Clone)</c> object.</description>
-        ///         </item>
-        ///     </list>
-        /// </remarks>
-        // protected internal abstract void OnGameStart();
-
-        /// <summary>
-        /// Called when a game ends, including when a player leaves a game in progress.
-        /// </summary>
-        /// <remarks>
-        /// This method should undo any persistant changes made during <see cref="OnGameStart"/>.
-        /// </remarks>
-        // protected internal abstract void OnGameEnd();
-
         /// <summary>
         /// Called when the rule is activated.
         /// </summary>
