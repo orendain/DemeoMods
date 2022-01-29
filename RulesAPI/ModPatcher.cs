e51949b0970abbbc2b1975c338c10c14518e6e93
Edgar Orendain <edgar@orendainx.com>
2022-01-29 00:11:39 -0800

    Find a better hook for recognizing when a game is started. (#62)
    * Find a better hook for recognizing when a game is started.

--- RulesAPI/ModPatcher.cs
+++ RulesAPI/ModPatcher.cs
@@ -1,46 +1,23 @@
 ﻿namespace RulesAPI
 {
-    using System.Reflection;
-    using Boardgame.Networking;
+    using Boardgame.BoardgameActions;
     using HarmonyLib;
 
     internal class ModPatcher
     {
-        private static bool _isCreatingGame;
-
         internal static void Patch(Harmony harmony)
         {
             harmony.Patch(
-                original: AccessTools.Inner(typeof(GameStateMachine), "CreatingGameState").GetTypeInfo().GetDeclaredMethod("Enter"),
-                prefix: new HarmonyMethod(typeof(ModPatcher), nameof(GameStateMachine_Enter_Prefix)));
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(GameStateMachine), "GoToPlayingState"),
-                postfix: new HarmonyMethod(typeof(ModPatcher), nameof(GameStateMachine_GoToPlayingState_Postfix)));
+                original: AccessTools.Method(typeof(BoardGameActionStartNewGame), "StartNewGame"),
+                postfix: new HarmonyMethod(typeof(ModPatcher), nameof(BoardGameActionStartNewGame_StartNewGame_Postfix)));
 
             // TODO(orendain): Hook into game ending events in order to deactivate activated rules.
         }
 
-        private static void GameStateMachine_Enter_Prefix(GameStateMachine gsm)
+        private static void BoardGameActionStartNewGame_StartNewGame_Postfix()
         {
-            var createGameMode = Traverse.Create(gsm).Field<CreateGameMode>("createGameMode").Value;
-            if (createGameMode != CreateGameMode.Private)
-            {
-                return;
-            }
-
-            _isCreatingGame = true;
-        }
-
-        private static void GameStateMachine_GoToPlayingState_Postfix()
-        {
-            // TODO(orendain): Finding more appropriate hook locations than GSM's GoToPlayingState,
-            // which is called at the end of BoardGameActionStartNewGame.
-            if (_isCreatingGame)
-            {
-                _isCreatingGame = false;
-                RulesAPI.ActivateSelectedRuleset();
-            }
+            // TODO(orendain): Reintroduce checks on private/public gamemode.
+            RulesAPI.ActivateSelectedRuleset();
         }
     }
 }
