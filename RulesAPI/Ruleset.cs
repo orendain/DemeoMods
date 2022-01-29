21ce5ed9d2629b784faab48bcf5ba67e41d2c2f5
Edgar Orendain <edgar@orendainx.com>
2022-01-29 01:39:02 -0800

    Flatten rule callback calls.
    Instead of RulesAPI class calling `Activate()` on a Ruleset, which in turn would call `Activate()` on the Rule baseclass, which in turn would call the implemented `OnActivate()`, RulesAPI calls the callbacks itself.

--- RulesAPI/Ruleset.cs
+++ RulesAPI/Ruleset.cs
@@ -1,6 +1,5 @@
 ﻿namespace RulesAPI
 {
-    using System;
     using System.Collections.Generic;
 
     public class Ruleset
@@ -30,40 +29,6 @@
             Name = name;
             Description = description;
             Rules = rules;
-        }
-
-        internal void Activate()
-        {
-            RulesAPI.Logger.Msg($"Activating ruleset: {Name} (with {Rules.Count} rules)");
-
-            foreach (var rule in Rules)
-            {
-                try
-                {
-                    rule.Activate();
-                }
-                catch (Exception e)
-                {
-                    // TODO(orendain): Rollback activation.
-                    RulesAPI.Logger.Warning($"Failed to activate rule [{Name}]: {e}");
-                }
-            }
-        }
-
-        internal void Deactivate()
-        {
-            RulesAPI.Logger.Msg($"Deactivating ruleset: {Name} (with {Rules.Count} rules)");
-            foreach (var rule in Rules)
-            {
-                try
-                {
-                    rule.Deactivate();
-                }
-                catch (Exception e)
-                {
-                    RulesAPI.Logger.Warning($"Failed to deactivate rule [{Name}]: {e}");
-                }
-            }
         }
     }
 }
