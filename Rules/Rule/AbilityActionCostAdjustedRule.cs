ae2eacaa4389ca0028b33c5ab61be4d8dd9d1bac
Jim Conner <noreply@github.com>
2022-01-31 13:35:17 +0000

    A rule for adjusting whether specific cards have a cost to cast or not.

---
+++ Rules/Rule/AbilityActionCostAdjustedRule.cs
@@ -0,0 +1,44 @@
+﻿namespace Rules.Rule
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Boardgame.BoardEntities.Abilities;
+    using UnityEngine;
+
+    public sealed class AbilityActionCostAdjustedRule : RulesAPI.Rule
+    {
+        public override string Description => "Ability AP costs are adjusted";
+
+        private readonly Dictionary<string, bool> _adjustments;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AbilityActionCostAdjustedRule"/> class.
+        /// </summary>
+        /// <param name="adjustments">Key-value pairs mapping the name of an entity to the number of action points
+        /// added to their base. Negative numbers are allowed.</param>
+        public AbilityActionCostAdjustedRule(Dictionary<string, bool> adjustments)
+        {
+            _adjustments = adjustments;
+        }
+
+        protected override void OnPostGameCreated()
+        {
+            var abilities = Resources.FindObjectsOfTypeAll<Ability>();
+            foreach (var item in _adjustments)
+            {
+                var ability = abilities.First(c => c.name.Equals($"{item.Key}(Clone)"));
+                ability.costActionPoint= item.Value;
+            }
+        }
+
+        protected override void OnDeactivate()
+        {
+            var abilities = Resources.FindObjectsOfTypeAll<Ability>();
+            foreach (var item in _adjustments)
+            {
+                var ability = abilities.First(c => c.name.Equals($"{item.Key}(Clone)"));
+                ability.costActionPoint = ! item.Value;
+            }
+        }
+    }
+}
