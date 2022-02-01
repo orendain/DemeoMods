6ee1c878c34791de5dc369de5eff1bcc8dabe4a1
Edgar Orendain <edgar@orendainx.com>
2022-01-31 18:22:40 -0800

    Introduced a temporary interface that can be implemented by rules that have the capability to produce strings that represent the configured rule.  E.g., that can serialize their state.
    Added a couple of corresponding classes that can write a ruleset to config, read a ruleset
    from config, and can orchestrate the parsing of configuration strings into instantiated rules.

---
+++ Rules/Rule/GoldPickedUpScaledRule.cs
@@ -0,0 +1,59 @@
+﻿namespace Rules.Rule
+{
+    using Boardgame.Data;
+    using Boardgame.SerializableEvents;
+    using HarmonyLib;
+    using MelonLoader.TinyJSON;
+
+    public sealed class GoldPickedUpScaledRule : RulesAPI.Rule, RulesAPI.IConfigurableRule
+    {
+        public override string Description => "Gold picked up is scaled";
+
+        private static Config _config;
+        private static bool _isActivated;
+
+        public struct Config
+        {
+            public float Multiplier;
+        }
+
+        public GoldPickedUpScaledRule(Config config)
+        {
+            _config = config;
+        }
+
+        public static GoldPickedUpScaledRule FromConfigString(string configString)
+        {
+            JSON.MakeInto(JSON.Load(configString), out Config conf);
+            return new GoldPickedUpScaledRule(conf);
+        }
+
+        public string ToConfigString()
+        {
+            return JSON.Dump(_config, EncodeOptions.NoTypeHints | EncodeOptions.PrettyPrint);
+        }
+
+        protected override void OnActivate() => _isActivated = true;
+
+        protected override void OnDeactivate() => _isActivated = false;
+
+        private static void OnPatch(Harmony harmony)
+        {
+            harmony.Patch(
+                original: AccessTools.Constructor(
+                    typeof(SerializableEventPickup),
+                    new[] { typeof(int), typeof(IntPoint2D), typeof(bool) }),
+                postfix: new HarmonyMethod(typeof(GoldPickedUpScaledRule), nameof(SerializableEventPickup_Constructor_Postfix)));
+        }
+
+        private static void SerializableEventPickup_Constructor_Postfix(ref SerializableEventPickup __instance)
+        {
+            if (!_isActivated)
+            {
+                return;
+            }
+
+            __instance.goldAmount = (int)(__instance.goldAmount * _config.Multiplier);
+        }
+    }
+}
---
+++ RulesAPI/IConfigurableRule.cs
@@ -0,0 +1,26 @@
+﻿namespace RulesAPI
+{
+    public interface IConfigurableRule
+    {
+        /// <summary>
+        /// Gets a string that can represents the rule's configuration.
+        /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///     The implementation of this method must guarantee that when the returned string is passed into the
+        ///     corresponding <c>FromConfigString</c>, it will produce a rule identical to the one that
+        ///     <c>ToConfigString</c> was called on.
+        ///     </para>
+        ///     <para>
+        ///     For portability purposes, it is recommended that the configuration string keep to UTF-8 encoding.
+        ///     </para>
+        /// </remarks>
+        /// <returns>a string that represents the rule's configuration.</returns>
+        string ToConfigString();
+
+        /// <summary>
+        /// Returns an instantiated rule initialized with the specified configuration string.
+        /// </summary>
+        // public static <the-rule-type> FromConfigString(string configString);
+    }
+}
