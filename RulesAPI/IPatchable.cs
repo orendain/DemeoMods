ca3104b6bc06b9721631afea56e74e70e57a3c9e
Edgar Orendain <edgar@orendainx.com>
2022-02-03 19:08:36 -0800

    Introduce the IPatchable interface that may implemented by Rules.

---
+++ RulesAPI/IPatchable.cs
@@ -0,0 +1,14 @@
+﻿namespace RulesAPI
+{
+    public interface IPatchable
+    {
+        /// <summary>
+        /// Patches the game with the given patcher.
+        /// </summary>
+        /// <remarks>
+        /// Rules must check the property <c>IsActivated</c> in the patched method to ensure it makes
+        /// modifications only when it is activated.
+        /// </remarks>
+        // private static void Patch(HarmonyLib.Harmony harmony);
+    }
+}
