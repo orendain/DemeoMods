35366c222c6fac577cac33a1d723d321327eb197
Edgar Orendain <edgar@orendainx.com>
2022-02-04 00:15:19 -0800

    Make CardSellValueMultipliedRule config writable.

--- Rules/Rule/CardSellValueMultipliedRule.cs
+++ Rules/Rule/CardSellValueMultipliedRule.cs
@@ -2,17 +2,29 @@
 {
     using System.Reflection;
     using HarmonyLib;
+    using MelonLoader.TinyJSON;
 
     public sealed class CardSellValueMultipliedRule : RulesAPI.Rule, RulesAPI.IPatchable
     {
         public override string Description => "Card sell values are multiplied";
 
-        private static double _multiplier;
+        private static float _multiplier;
         private static bool _isActivated;
 
-        public CardSellValueMultipliedRule(double multiplier)
+        public CardSellValueMultipliedRule(float multiplier)
         {
             _multiplier = multiplier;
+        }
+
+        public static CardSellValueMultipliedRule FromConfigString(string configString)
+        {
+            JSON.MakeInto(JSON.Load(configString), out float conf);
+            return new CardSellValueMultipliedRule(conf);
+        }
+
+        public string ToConfigString()
+        {
+            return JSON.Dump(_multiplier, EncodeOptions.NoTypeHints);
         }
 
         protected override void OnActivate() => _isActivated = true;
@@ -22,9 +34,13 @@
         private static void Patch(Harmony harmony)
         {
             harmony.Patch(
-                original: AccessTools.Inner(typeof(CardShopController), "CardShopInfoProvider").GetTypeInfo()
+                original: AccessTools
+                    .Inner(typeof(CardShopController), "CardShopInfoProvider")
+                    .GetTypeInfo()
                     .GetDeclaredMethod("GetCardSellValue"),
-                postfix: new HarmonyMethod(typeof(CardSellValueMultipliedRule), nameof(CardShopInfoProvider_GetCardSellValue_Postfix)));
+                postfix: new HarmonyMethod(
+                    typeof(CardSellValueMultipliedRule),
+                    nameof(CardShopInfoProvider_GetCardSellValue_Postfix)));
         }
 
         private static void CardShopInfoProvider_GetCardSellValue_Postfix(ref int __result)
