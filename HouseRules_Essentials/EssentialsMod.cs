f1470d2f7c2a55d0d09108620100b616e526b535
Edgar Orendain <edgar@orendainx.com>
2022-02-07 10:45:17 -0800

    Remove unneeded (?) rules.

--- HouseRules_Essentials/EssentialsMod.cs
+++ HouseRules_Essentials/EssentialsMod.cs
@@ -34,10 +34,8 @@
             registrar.Register(typeof(Rules.LevelPropertiesModifiedRule));
             registrar.Register(typeof(Rules.PieceConfigAdjustedRule));
             registrar.Register(typeof(Rules.RatNestsSpawnGoldRule));
-            registrar.Register(typeof(Rules.SorcererStartCardsModifiedRule));
             registrar.Register(typeof(Rules.StartCardsModifiedRule));
             registrar.Register(typeof(Rules.StartHealthAdjustedRule));
-            registrar.Register(typeof(Rules.ZapStartingInventoryAdjustedRule));
         }
 
         private static void RegisterNewRulesets()
