905c1b900819c53c4b3cc659287a3681606eba8d
Edgar Orendain <edgar@orendainx.com>
2022-02-09 14:36:27 -0800

    Quick initial draft of UI.

---
+++ HouseRules_Configuration/UI/RulesetSelectionPanel.cs
@@ -0,0 +1,86 @@
+﻿namespace HouseRules.Configuration.UI
+{
+    using System;
+    using System.Linq;
+    using Common.UI;
+    using HouseRules.Types;
+    using UnityEngine;
+
+    internal class RulesetSelectionPanel
+    {
+        private readonly UiHelper _uiHelper;
+
+        private readonly Rulebook _rulebook;
+
+        internal GameObject GameObject { get; }
+
+        internal static RulesetSelectionPanel NewInstance(Rulebook rulebook, UiHelper uiHelper)
+        {
+            return new RulesetSelectionPanel(rulebook, uiHelper, new GameObject("RulesetSelectionPanel"));
+        }
+
+        private RulesetSelectionPanel(Rulebook rulebook, UiHelper uiHelper, GameObject panel)
+        {
+            this._rulebook = rulebook;
+            this._uiHelper = uiHelper;
+            this.GameObject = panel;
+
+            Render();
+        }
+
+        private void Render()
+        {
+            var headerContainer = CreateHeader();
+            headerContainer.transform.SetParent(GameObject.transform, worldPositionStays: false);
+
+            var rulesetsContainer = new GameObject("Rulesets");
+            rulesetsContainer.transform.SetParent(GameObject.transform, worldPositionStays: false);
+            rulesetsContainer.transform.localPosition = new Vector3(0, -1f, 0);
+
+            for (var i = 0; i < _rulebook.Rulesets.Count; i++)
+            {
+                var yOffset = i * -3f;
+                var rulesetRow = CreateRulesetRow(_rulebook.Rulesets.ElementAt(i));
+                rulesetRow.transform.SetParent(rulesetsContainer.transform, worldPositionStays: false);
+                rulesetRow.transform.localPosition = new Vector3(0, yOffset, 0);
+            }
+        }
+
+        private GameObject CreateHeader()
+        {
+            var headerContainer = new GameObject("RulesetSelectionHeader");
+
+            var infoText =
+                _uiHelper.CreateLabelText("Select a ruleset for your next private multiplayer game or skirmish.");
+            infoText.transform.SetParent(headerContainer.transform, worldPositionStays: false);
+            infoText.transform.localPosition = new Vector3(-1.5f, 0.3f, 0);
+
+            return headerContainer;
+        }
+
+        private GameObject CreateRulesetRow(Ruleset ruleset)
+        {
+            var roomRowContainer = new GameObject(ruleset.Name);
+
+            var button = _uiHelper.CreateButton(SelectRulesetAction(ruleset.Name));
+            button.transform.SetParent(roomRowContainer.transform, worldPositionStays: false);
+            button.transform.localScale = new Vector3(1, 1, 1);
+            button.transform.localPosition = new Vector3(0, 0, 0);
+
+            var buttonText = _uiHelper.CreateText(ruleset.Name, Color.white, UiHelper.DefaultLabelFontSize);
+            buttonText.transform.SetParent(roomRowContainer.transform, worldPositionStays: false);
+            buttonText.transform.localPosition = new Vector3(0, 0, 0);
+
+            var descriptionLabel = _uiHelper.CreateLabelText(ruleset.Description);
+            descriptionLabel.transform.SetParent(roomRowContainer.transform, worldPositionStays: false);
+            descriptionLabel.transform.localPosition = new Vector3(0, -1.2f, 0);
+
+            return roomRowContainer;
+        }
+
+        private static Action SelectRulesetAction(string rulesetName)
+        {
+            return () => HR.SelectRuleset(rulesetName);
+        }
+    }
+}
---
+++ HouseRules_Configuration/UI/RulesetSelectionUI.cs
@@ -0,0 +1,63 @@
+﻿namespace HouseRules.Configuration.UI
+{
+    using System.Collections;
+    using Common.UI;
+    using UnityEngine;
+
+    internal class RulesetSelectionUI : MonoBehaviour
+    {
+        private UiHelper _uiHelper;
+        private GameObject _background;
+        private RulesetSelectionPanel _rulesetSelectionPanel;
+
+        private void Start()
+        {
+            StartCoroutine(WaitAndInitialize());
+        }
+
+        private IEnumerator WaitAndInitialize()
+        {
+            while (!UiHelper.IsReady())
+            {
+                ConfigurationMod.Logger.Msg("UI helper not yet ready. Trying again...");
+                yield return new WaitForSecondsRealtime(1);
+            }
+
+            ConfigurationMod.Logger.Msg("UI helper ready. Proceeding with initialization.");
+
+            _uiHelper = UiHelper.Instance();
+            _rulesetSelectionPanel = RulesetSelectionPanel.NewInstance(HR.Rulebook, _uiHelper);
+            Initialize();
+
+            ConfigurationMod.Logger.Msg("Initialization complete.");
+        }
+
+        private void Initialize()
+        {
+            this.transform.SetParent(_uiHelper.DemeoResource.LobbyAnchor.transform, worldPositionStays: true);
+            this.transform.position = new Vector3(-25, 30, 0);
+            this.transform.rotation = Quaternion.Euler(0, -40, 0);
+
+            _background = new GameObject("RulesetSelectionUIBackground");
+            _background.AddComponent<MeshFilter>().mesh = _uiHelper.DemeoResource.MenuBoxMesh;
+            _background.AddComponent<MeshRenderer>().material = _uiHelper.DemeoResource.MenuBoxMaterial;
+
+            _background.transform.SetParent(this.transform, worldPositionStays: false);
+            _background.transform.localPosition = new Vector3(0, -3.6f, 0);
+            _background.transform.localRotation =
+                Quaternion.Euler(-90, 0, 0); // Un-flip card from it's default face-up position.
+            _background.transform.localScale = new Vector3(2, 1, 2.5f);
+
+            var menuTitle = _uiHelper.CreateMenuHeaderText("HouseRules");
+            menuTitle.transform.SetParent(this.transform, worldPositionStays: false);
+            menuTitle.transform.localPosition = new Vector3(0, 2.375f, 0);
+
+            var selectionPanel = _rulesetSelectionPanel.GameObject;
+            selectionPanel.transform.SetParent(this.transform, worldPositionStays: false);
+            selectionPanel.transform.localPosition = new Vector3(-0, -1f, 0);
+
+            // TODO(orendain): Fix so that ray interacts with entire object.
+            this.gameObject.AddComponent<BoxCollider>();
+        }
+    }
+}
