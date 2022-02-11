ea5f648019b0e1f1222c11d4ea839b1a223cf087
Chris Templeman <cl.templeman@gmail.com>
2022-02-11 00:54:15 -0500

    additional ruleset 'NoSurprises'

---
+++ HouseRules_Essentials/Rulesets/NoSurprises.cs
@@ -0,0 +1,20 @@
+namespace HouseRules.Essentials.Rulesets
+{
+    using HouseRules.Essentials.Rules;
+    using HouseRules.Types;
+
+    internal static class NoSurprisesRuleset
+    {
+        internal static Ruleset Create()
+        {
+            const string name = "No Surprises.";
+            const string description = "This prevents any surprises in the dark or coming through doors.";
+
+            return Ruleset.NewInstance(
+                name,
+                description,
+                new EnemyDoorOpeningDisabledRule(true),
+                new EnemyRespawnDisabledRule(true),
+        }
+    }
+}
