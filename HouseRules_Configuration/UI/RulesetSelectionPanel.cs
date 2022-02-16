25add3552b84b867dadb8e90a5989acdb214bdad
Edgar Orendain <edgar@orendainx.com>
2022-02-16 13:34:22 -0800

    Modify HouseRules UI.

--- HouseRules_Configuration/UI/RulesetSelectionPanel.cs
+++ HouseRules_Configuration/UI/RulesetSelectionPanel.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Linq;
     using Common.UI;
+    using DataKeys;
     using HouseRules.Types;
     using UnityEngine;
 
@@ -35,7 +36,7 @@
 
             var rulesetsContainer = new GameObject("Rulesets");
             rulesetsContainer.transform.SetParent(GameObject.transform, worldPositionStays: false);
-            rulesetsContainer.transform.localPosition = new Vector3(0, -2f, 0);
+            rulesetsContainer.transform.localPosition = new Vector3(0, -1.5f, 0);
 
             var rulesetRow = CreateRulesetRow(Ruleset.None);
             rulesetRow.transform.SetParent(rulesetsContainer.transform, worldPositionStays: false);
@@ -43,7 +44,7 @@
 
             for (var i = 0; i < _rulebook.Rulesets.Count; i++)
             {
-                var yOffset = (i + 1) * -3f;
+                var yOffset = (i + 1) * -1.75f;
                 rulesetRow = CreateRulesetRow(_rulebook.Rulesets.ElementAt(i));
                 rulesetRow.transform.SetParent(rulesetsContainer.transform, worldPositionStays: false);
                 rulesetRow.transform.localPosition = new Vector3(0, yOffset, 0);
@@ -52,11 +53,13 @@
 
         private GameObject CreateHeader()
         {
-            var headerContainer = new GameObject("RulesetSelectionHeader");
+            var headerContainer = new GameObject("RulesetsHeader");
 
-            var infoText = _uiHelper.CreateLabelText($"Select a ruleset for your next\nprivate multiplayer game or skirmish.");
-            infoText.transform.SetParent(headerContainer.transform, worldPositionStays: false);
-            infoText.transform.localPosition = new Vector3(0f, 1f, 0);
+            var infoText = _uiHelper.CreateLabelText("Select a ruleset for your next private multiplayer game or skirmish.");
+            var rectTransform = (RectTransform)infoText.transform;
+            rectTransform.SetParent(headerContainer.transform, worldPositionStays: false);
+            rectTransform.sizeDelta = new Vector2(10, 2);
+            rectTransform.localPosition = new Vector3(0f, 1f, UiHelper.DefaultTextZShift);
 
             return headerContainer;
         }
@@ -67,14 +70,19 @@
 
             var button = _uiHelper.CreateButton(SelectRulesetAction(ruleset.Name));
             button.transform.SetParent(roomRowContainer.transform, worldPositionStays: false);
-            button.transform.localScale = new Vector3(2f, 2f, 1);
-            button.transform.localPosition = new Vector3(0, 0, 0);
+            button.transform.localScale = new Vector3(1.5f, 1f, 1);
+            button.transform.localPosition = new Vector3(-4.5f, 0, 0);
 
-            var buttonTextString = $"{ruleset.Name}\n{ruleset.Description}";
-
-            var buttonText = _uiHelper.CreateText(buttonTextString, Color.white, UiHelper.DefaultLabelFontSize);
+            var buttonText = _uiHelper.CreateText(ruleset.Name, Color.white, UiHelper.DefaultLabelFontSize);
             buttonText.transform.SetParent(roomRowContainer.transform, worldPositionStays: false);
-            buttonText.transform.localPosition = new Vector3(0, 0, 0);
+            buttonText.transform.localPosition = new Vector3(-4.5f, 0, UiHelper.DefaultTextZShift);
+
+            var description = _uiHelper.CreateLabelText(ruleset.Description);
+            var rectTransform = (RectTransform)description.transform;
+            rectTransform.SetParent(roomRowContainer.transform, worldPositionStays: false);
+            rectTransform.pivot = Vector2.left;
+            rectTransform.sizeDelta = new Vector2(9, 1);
+            rectTransform.localPosition = new Vector3(-9.7f, -0.5f, UiHelper.DefaultTextZShift);
 
             return roomRowContainer;
         }
