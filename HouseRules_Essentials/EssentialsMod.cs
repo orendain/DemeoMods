d321f35221ccc4c64c11b1eaadde7b84d84500bc
Edgar Orendain <edgar@orendainx.com>
2022-02-17 04:26:09 -0800

    Tweak BeatTheClock ruleset. (#171)
    Reduced HP to 200 to make health depletion at least somewhat more interesting. Reduced round count to 15 to increase difficulty. Decreatsed card recycling to reduce spam. Added POIs to increase difficulty by way of hiding the exit.

--- HouseRules_Essentials/EssentialsMod.cs
+++ HouseRules_Essentials/EssentialsMod.cs
@@ -43,7 +43,6 @@
 
         private static void RegisterRulesets()
         {
-            HR.Rulebook.Register(SampleRuleset.Create());
             HR.Rulebook.Register(NoSurprisesRuleset.Create());
             HR.Rulebook.Register(BeatTheClockRuleset.Create());
             HR.Rulebook.Register(DifficultyEasyRuleset.Create());
