ad86e4fe639c1e935715a597e633a957f0925e84
Jim Conner <noreply@github.com>
2022-02-17 17:01:39 +0000

    Accept replacement values for PieceConfig instead of modifiers.

--- HouseRules_Configuration/ConfigurationMod.cs
+++ HouseRules_Configuration/ConfigurationMod.cs
@@ -106,21 +106,21 @@
             var gpus = new Essentials.Rules.GoldPickedUpMultipliedRule(2f);
             var pca = new Essentials.Rules.PieceConfigAdjustedRule(new List<List<string>>
             {
-                new List<string> { "HeroSorcerer", "MoveRange", "1" }, // 1 more movement range
-                new List<string> { "HeroSorcerer", "StartHealth", "10" }, // 10 extra HP
-                new List<string> { "HeroGuardian", "MoveRange", "1" },
-                new List<string> { "HeroGuardian", "StartHealth", "10" },
-                new List<string> { "HeroHunter", "MoveRange", "1" },
-                new List<string> { "HeroHunter", "StartHealth", "10" },
-                new List<string> { "HeroBard", "MoveRange", "1" },
-                new List<string> { "HeroBard", "StartHealth", "10" },
-                new List<string> { "HeroRouge", "MoveRange", "1" },
-                new List<string> { "HeroRouge", "StartHealth", "10" },
+                new List<string> { "HeroSorcerer", "MoveRange", "6" }, // 1 more movement range
+                new List<string> { "HeroSorcerer", "StartHealth", "20" }, // 10 extra HP
+                new List<string> { "HeroGuardian", "MoveRange", "5" },
+                new List<string> { "HeroGuardian", "StartHealth", "15" },
+                new List<string> { "HeroHunter", "MoveRange", "6" },
+                new List<string> { "HeroHunter", "StartHealth", "18" },
+                new List<string> { "HeroBard", "MoveRange", "8" },
+                new List<string> { "HeroBard", "StartHealth", "12" },
+                new List<string> { "HeroRouge", "MoveRange", "5" },
+                new List<string> { "HeroRouge", "StartHealth", "20" },
                 new List<string> { "WolfCompanion", "StartHealth", "20" }, // Wolf wastes this many HP wandering through gas
-                new List<string> { "SwordOfAvalon", "StartHealth", "10" },
-                new List<string> { "BeaconOfSmite", "StartHealth", "10" },
-                new List<string> { "BeaconOfSmite", "ActionPoint", "1" }, // Behemoth gets to fire two rounds
-                new List<string> { "MonsterBait", "StartHealth", "20" }, // Lure needs to last a little longer
+                new List<string> { "SwordOfAvalon", "StartHealth", "20" },
+                new List<string> { "BeaconOfSmite", "StartHealth", "25" },
+                new List<string> { "BeaconOfSmite", "ActionPoint", "2" }, // Behemoth gets to fire two rounds
+                new List<string> { "MonsterBait", "StartHealth", "25" }, // Lure needs to last a little longer
             });
             var rnsg = new Essentials.Rules.RatNestsSpawnGoldRule(8);
             var cards = new List<Essentials.Rules.StartCardsModifiedRule.CardConfig>
--- HouseRules_Essentials/Rules/PieceConfigAdjustedRule.cs
+++ HouseRules_Essentials/Rules/PieceConfigAdjustedRule.cs
@@ -3,6 +3,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using Boardgame;
+    using DataKeys;
     using HarmonyLib;
     using HouseRules.Types;
     using UnityEngine;
@@ -12,15 +13,17 @@
         public override string Description => "Piece configuration is adjusted";
 
         private readonly List<List<string>> _adjustments;
+        private readonly List<List<string>> _originals;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PieceConfigAdjustedRule"/> class.
         /// </summary>
-        /// <param name="adjustments">Lists of PieceNames, PropertyNames and Values
-        /// added to their base. Negative numbers are allowed.</param>
+        /// <param name="adjustments">String lists of PieceNames, PropertyNames and integer values
+        /// replacing defaults.</param>
         public PieceConfigAdjustedRule(List<List<string>> adjustments)
         {
             _adjustments = adjustments;
+            _originals = new List<List<string>>();
         }
 
         public List<List<string>> GetConfigObject() => _adjustments;
@@ -32,18 +35,19 @@
             {
                 var pieceConfig = pieceConfigs.First(c => c.name.Equals($"PieceConfig_{item[0]}"));
                 var property = Traverse.Create(pieceConfig).Property<int>(item[1]);
-                property.Value += int.Parse(item[2]);
+                _originals.Add (new List<string> { item[0], item[1], property.Value.ToString() });
+                property.Value = int.Parse(item[2]);
             }
         }
 
         protected override void OnDeactivate(GameContext gameContext)
         {
             var pieceConfigs = Resources.FindObjectsOfTypeAll<PieceConfig>();
-            foreach (var item in _adjustments)
+            foreach (var item in _originals)
             {
                 var pieceConfig = pieceConfigs.First(c => c.name.Equals($"PieceConfig_{item[0]}"));
                 var property = Traverse.Create(pieceConfig).Property<int>(item[1]);
-                property.Value -= int.Parse(item[2]);
+                property.Value = int.Parse(item[2]);
             }
         }
     }
