699d81fa048ec1356f437228826eaff0d23254c0
Jim Conner <noreply@github.com>
2022-03-14 20:26:53 +0000

    UseWhenKilled ability list rule

--- HouseRules_Essentials/EssentialsMod.cs
+++ HouseRules_Essentials/EssentialsMod.cs
@@ -41,6 +41,7 @@
             HR.Rulebook.Register(typeof(PieceAbilityListOverriddenRule));
             HR.Rulebook.Register(typeof(PieceBehavioursListOverriddenRule));
             HR.Rulebook.Register(typeof(PiecePieceTypeListOverriddenRule));
+            HR.Rulebook.Register(typeof(PieceUseWhenKilledOverriddenRule));
             HR.Rulebook.Register(typeof(RatNestsSpawnGoldRule));
             HR.Rulebook.Register(typeof(RegainAbilityIfMaxxedOutOverriddenRule));
             HR.Rulebook.Register(typeof(RoundCountLimitedRule));
