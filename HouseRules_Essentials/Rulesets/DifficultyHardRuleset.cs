22ad7a7694f54bd3ad4d5c7d2635e2ad85c555cf
Buruko <62775380+Buruko@users.noreply.github.com>
2022-03-17 20:35:36 -0400

    Difficulty Adjustments (#231)
    * Adjusted HP/ATK scaling, added LevelPropertiesModified to reduce available resources on higher Difficulty
    
    * Corrections and clean up to variable settings
    
    * Added Max Gold Piles for Hard and Legendary further limiting resources
    
    Co-authored-by: Chris Templeman <cl.templeman@gmail.com>

--- HouseRules_Essentials/Rulesets/DifficultyHardRuleset.cs
+++ HouseRules_Essentials/Rulesets/DifficultyHardRuleset.cs
@@ -10,13 +10,32 @@
             const string name = "Difficulty: Hard";
             const string description = "Increased game difficulty for a greater challenge.";
 
+            var cardEnergyAttck = new CardEnergyFromAttackMultipliedRule(0.8f);
+            var cardEnegyRecycle = new CardEnergyFromRecyclingMultipliedRule(0.8f);
+            var EnemyScaleHealth = new EnemyHealthScaledRule(1.8f);
+            var EnemyScaleAttack = new EnemyAttackScaledRule(1.5f);
+
+            var levelPropertiesModified = new LevelPropertiesModifiedRule(new Dictionary<string, int>
+            {
+              { "BigGoldPileChance", 30 },
+              { "FloorOneHealingFountains", 1 },
+              { "FloorOneLootChests", 2 },
+              { "FloorOneGoldMaxAmount", 672},
+              { "FloorTwoHealingFountains", 1 },
+              { "FloorTwoLootChests", 2 },
+              { "FloorTwoGoldMaxAmount", 420},
+              { "FloorThreeHealingFountains", 1 },
+              { "FloorThreeLootChests", 1 },
+            });
+
             return Ruleset.NewInstance(
                 name,
                 description,
-                new CardEnergyFromAttackMultipliedRule(0.8f),
-                new CardEnergyFromRecyclingMultipliedRule(0.8f),
-                new EnemyHealthScaledRule(1.2f),
-                new EnemyAttackScaledRule(1.2f));
+                cardEneryAttack,
+                cardEnergyRecycle,
+                EnemyScaleAttack,
+                EnemyScaleHealth,
+                levelPropertiesRule);
         }
     }
 }
--- HouseRules_Essentials/Rulesets/DifficultyLegendaryRuleset.cs
+++ HouseRules_Essentials/Rulesets/DifficultyLegendaryRuleset.cs
@@ -10,13 +10,32 @@
             const string name = "Difficulty: Legendary";
             const string description = "Increased game difficulty for those who want to be a legend.";
 
+            var levelProperties = new LevelPropertiesModifiedRule(new Dictionary<string, int>
+            {
+                { "BigGoldPileChance", 10 },
+                { "FloorOneHealingFountains", 0 },
+                { "FloorOneLootChests", 1 },
+                { "FloorOneGoldMaxAmount", 504},
+                { "FloorTwoHealingFountains", 1 },
+                { "FloorTwoLootChests", 1 },
+                { "FloorTwoGoldMaxAmount", 252},
+                { "FloorThreeHealingFountains", 0 },
+                { "FloorThreeLootChests", 1 },
+            });
+
+            var cardEneryAttack = new CardEnergyFromAttackMultipliedRule(0.5f);
+            var cardEnergyRecycle = new CardEnergyFromRecyclingMultipliedRule(0.5f);
+            var EnemyScaleHealth = new EnemyHealthScaledRule(2.2f);
+            var EnemyScaleAttack = new EnemyAttackScaledRule(2.0f);
+
             return Ruleset.NewInstance(
                 name,
                 description,
-                new CardEnergyFromAttackMultipliedRule(0.6f),
-                new CardEnergyFromRecyclingMultipliedRule(0.6f),
-                new EnemyHealthScaledRule(1.4f),
-                new EnemyAttackScaledRule(1.4f));
+                cardEneryAttack,
+                cardEnergyRecycle,
+                EnemyScaleAttack,
+                EnemyScaleHealth,
+                levelProperties);
         }
     }
 }
