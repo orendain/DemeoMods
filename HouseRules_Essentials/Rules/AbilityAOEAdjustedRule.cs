42fda697df26b2aded4b32f9cb40d2234dd21a87
Edgar Orendain <edgar@orendainx.com>
2022-03-18 22:18:04 -0700

    Modify AbilityAoE rule to perform an abilityKey-to-ability lookup instead of searching for specifically-named Unity resources.

--- HouseRules_Essentials/Rules/AbilityAOEAdjustedRule.cs
+++ HouseRules_Essentials/Rules/AbilityAOEAdjustedRule.cs
@@ -1,53 +1,60 @@
 ﻿namespace HouseRules.Essentials.Rules
 {
     using System.Collections.Generic;
-    using System.Linq;
     using Boardgame;
     using Boardgame.BoardEntities.Abilities;
+    using DataKeys;
     using HarmonyLib;
     using HouseRules.Types;
-    using UnityEngine;
 
-    public sealed class AbilityAoeAdjustedRule : Rule, IConfigWritable<Dictionary<string, int>>, IMultiplayerSafe
+    public sealed class AbilityAoeAdjustedRule : Rule, IConfigWritable<Dictionary<AbilityKey, int>>, IMultiplayerSafe
     {
         public override string Description => "Ability AOE Ranges are adjusted";
 
-        private readonly Dictionary<string, int> _adjustments;
+        private readonly Dictionary<AbilityKey, int> _adjustments;
+        private Dictionary<AbilityKey, int> _originals;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AbilityAoeAdjustedRule"/> class.
         /// </summary>
         /// <param name="adjustments">Key-value pairs mapping the name of an ability and the AOE range
         /// added to their base. Adding '1' to a 3x3 spell will make a 5x5. Negative values will reduce AOE.</param>
-        public AbilityAoeAdjustedRule(Dictionary<string, int> adjustments)
+        public AbilityAoeAdjustedRule(Dictionary<AbilityKey, int> adjustments)
         {
             _adjustments = adjustments;
+            _originals = new Dictionary<AbilityKey, int>();
         }
 
-        public Dictionary<string, int> GetConfigObject() => _adjustments;
+        public Dictionary<AbilityKey, int> GetConfigObject() => _adjustments;
 
-        protected override void OnPostGameCreated(GameContext gameContext)
+        protected override void OnPreGameCreated(GameContext gameContext)
         {
-            var abilities = Resources.FindObjectsOfTypeAll<Ability>();
-            foreach (var item in _adjustments)
-            {
-                var ability = abilities.First(c => c.name.Equals($"{item.Key}(Clone)"));
-                var aoe = Traverse.Create(ability).Field<AreaOfEffect>("areaOfEffect").Value;
-                Traverse.Create(aoe).Field<int>("range").Value += item.Value; // Adjust the yellow AOE outline when casting.
-                ability.areaOfEffectRange += item.Value; // Adjust value displayed on the card.
-            }
+            _originals = ReplaceAbility(_adjustments);
         }
 
         protected override void OnDeactivate(GameContext gameContext)
         {
-            var abilities = Resources.FindObjectsOfTypeAll<Ability>();
-            foreach (var item in _adjustments)
+            ReplaceAbility(_originals);
+        }
+
+        private static Dictionary<AbilityKey, int> ReplaceAbility(Dictionary<AbilityKey, int> replacements)
+        {
+            var originals = new Dictionary<AbilityKey, int>();
+
+            foreach (var replacement in replacements)
             {
-                var ability = abilities.First(c => c.name.Equals($"{item.Key}(Clone)"));
+                if (!AbilityFactory.TryGetAbility(replacement.Key, out var ability))
+                {
+                    EssentialsMod.Logger.Warning($"Provided AbilityKey [{replacement.Key}] does not have a corresponding ability. Skipping that ability.");
+                }
+
+                originals[replacement.Key] = ability.areaOfEffectRange;
                 var aoe = Traverse.Create(ability).Field<AreaOfEffect>("areaOfEffect").Value;
-                Traverse.Create(aoe).Field<int>("range").Value -= item.Value; // Adjust the yellow AOE outline when casting.
-                ability.areaOfEffectRange -= item.Value; // Adjust value displayed on the card.
+                Traverse.Create(aoe).Field<int>("range").Value += replacement.Value; // Adjust the AOE outline when casting.
+                ability.areaOfEffectRange += replacement.Value; // Adjust value displayed on the card.
             }
+
+            return originals;
         }
     }
 }
