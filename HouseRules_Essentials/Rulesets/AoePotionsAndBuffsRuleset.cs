db0e372381fcc833d314dec9ff06f26e001827ee
Edgar Orendain <edgar@orendainx.com>
2022-03-20 06:40:21 -0700

    Codify 3x3 Potions and Buffs ruleset.

---
+++ HouseRules_Essentials/Rulesets/AoePotionsAndBuffsRuleset.cs
@@ -0,0 +1,39 @@
+﻿namespace HouseRules.Essentials.Rulesets
+{
+    using System.Collections.Generic;
+    using DataKeys;
+    using HouseRules.Essentials.Rules;
+    using HouseRules.Types;
+
+    internal static class AoePotionsAndBuffsRuleset
+    {
+        internal static Ruleset Create()
+        {
+            const string name = "3x3 Potions and Buffs";
+            const string description = "Heal, Strength, Speed, Adamant, Antitoxin, RepairArmor and Bard buffs are 3x3 AOE.";
+
+            var abilityAoeRule = new AbilityAoeAdjustedRule(new Dictionary<AbilityKey, int>
+            {
+                { AbilityKey.CourageShanty, 1 },
+                { AbilityKey.ReplenishArmor, 1 },
+                { AbilityKey.StrengthPotion, 1 },
+                { AbilityKey.SwiftnessPotion, 1 },
+                { AbilityKey.Antitoxin, 1 },
+                { AbilityKey.AdamantPotion, 1 },
+                { AbilityKey.HealingPotion, 1 },
+            });
+
+            var abilityMaxedRule = new RegainAbilityIfMaxxedOutOverriddenRule(new Dictionary<AbilityKey, bool>
+            {
+                { AbilityKey.StrengthPotion, false },
+                { AbilityKey.SwiftnessPotion, false },
+            });
+
+            return Ruleset.NewInstance(
+                name,
+                description,
+                abilityAoeRule,
+                abilityMaxedRule);
+        }
+    }
+}
