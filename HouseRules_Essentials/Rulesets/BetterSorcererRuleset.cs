fee64cd46a85cdc7d23f99708fcdb7fc26c67a2d
Edgar Orendain <edgar@orendainx.com>
2022-03-20 06:42:09 -0700

    Codify Better Sorcerer ruleset.

---
+++ HouseRules_Essentials/Rulesets/BetterSorcererRuleset.cs
@@ -0,0 +1,26 @@
+﻿namespace HouseRules.Essentials.Rulesets
+{
+    using System.Collections.Generic;
+    using DataKeys;
+    using HouseRules.Essentials.Rules;
+    using HouseRules.Types;
+
+    internal static class BetterSorcererRuleset
+    {
+        internal static Ruleset Create()
+        {
+            const string name = "Better Sorcerer";
+            const string description = "0 Action Cost for Sorcerer's Zap - No other changes. #STS";
+
+            var abilityActionCostRule = new AbilityActionCostAdjustedRule(new Dictionary<AbilityKey, bool>
+            {
+                { AbilityKey.Zap, false },
+            });
+
+            return Ruleset.NewInstance(
+                name,
+                description,
+                abilityActionCostRule);
+        }
+    }
+}
