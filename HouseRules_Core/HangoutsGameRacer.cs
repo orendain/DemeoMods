048c810064a1150d7eeb22b3468f7663c6a2f2e0
Edgar Orendain <edgar@orendainx.com>
2022-03-31 01:46:45 -0700

    Enable HangoutsGameRacer optimizations only when a ruleset is selected and for non-table hosts. (#269)
    * Only optimize race path when user has a ruleset selected.
    
    * Restructured functions to allow non-table-hosts to receive out-of-hangouts-path optimizations.
    
    * Only display time-to-join when it was being timed.
    
    * Reduce enforced delay before room creation down from 500ms to 50ms.
    
    * Add log message when joining the race as a not-table-host.

--- HouseRules_Core/HangoutsGameRacer.cs
+++ HouseRules_Core/HangoutsGameRacer.cs
@@ -1,13 +1,16 @@
 ﻿namespace HouseRules
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
     using System.Reflection;
+    using System.Reflection.Emit;
     using Boardgame.Social;
     using Bowser;
     using Bowser.Core;
     using Bowser.GameIntegration;
     using HarmonyLib;
+    using HouseRules.Types;
     using Photon.Pun;
 
     internal static class HangoutsGameRacer
@@ -17,7 +20,7 @@
         private static GameStateHobbyShop _gameStateHobbyShop;
         private static GameStateHobbyShopData _coroutineSpinner;
 
-        private static bool _isStartingHangoutsGame;
+        private static bool _hasJoinedTheRace;
         private static JoinParameters _joinParameters;
 
         internal static void Patch(Harmony harmony)
@@ -51,6 +54,11 @@
                     .Inner(typeof(GameStateMachine), "CreatingGameState").GetTypeInfo()
                     .GetDeclaredMethod("OnJoinedRoom"),
                 prefix: new HarmonyMethod(typeof(HangoutsGameRacer), nameof(CreatingGameState_OnJoinedRoom_Prefix)));
+
+            harmony.Patch(
+                original: AccessTools
+                    .Method(typeof(PlayWithFriendsController).GetNestedType("<TryCreateTheRoom>d__25"), "MoveNext"),
+                transpiler: new HarmonyMethod(typeof(HangoutsGameRacer), nameof(PlayWithFriendsController_TryCreateTheRoom_Transpiler)));
         }
 
         private static void GameStateHobbyShop_Start_Postfix(GameStateHobbyShop __instance, GameStateData baseData)
@@ -61,55 +69,51 @@
 
         private static bool GroupLaunchTable_OnStartButtonPressed_Prefix(GroupLaunchTable __instance)
         {
-            HR.Logger.Msg("[HangoutGameRacer] Attempting to race out of Hangouts by front-loading computation.");
-            _isStartingHangoutsGame = true;
-
-            var moduleType = FindSelectedModelType(__instance);
-            var destination = ConvertModuleTypeToDestination(moduleType);
-            var uniqueId = Guid.NewGuid().ToString();
-
-            var stringToRoomCode = new StringToRoomCode(_coroutineSpinner);
-            stringToRoomCode.GetRoomCodeFromString(uniqueId, delegate(string roomCode)
+            if (HR.SelectedRuleset == Ruleset.None)
             {
-                _joinParameters = new JoinParameters();
-                _joinParameters.groupId = GetPareaRoomCode(roomCode);
-                _joinParameters.gameId = string.Empty;
-                _joinParameters.destination = destination;
+                return true;
+            }
 
-                HR.Logger.Msg("[HangoutGameRacer] Pre-fetched room code.");
-                NotifyPlayersAndLeaveHangouts(__instance, _joinParameters.groupId, moduleType);
-            });
+            HR.Logger.Msg("[HangoutGameRacer] Attempting to race out of Hangouts as table host by front-loading computation.");
+            _hasJoinedTheRace = true;
+
+            var groupId = Guid.NewGuid().ToString();
+            var moduleType = FindSelectedModelType(__instance);
+            RaceOutOfHangouts(__instance, groupId, moduleType, isTableHost: true);
 
             return false;
         }
 
-        private static void NotifyPlayersAndLeaveHangouts(GroupLaunchTable groupLaunchTable, string groupId, GroupLaunchModuleData.ModuleType moduleType)
-        {
-            HR.Logger.Msg("[HangoutGameRacer] Finally notifying players of race.");
-            Traverse.Create(groupLaunchTable).Field<bool>("leavingBowser").Value = true;
-
-            // Recreating `GameStateHobbyShop.ExitBowser`
-            var teleport = Traverse.Create(_gameStateHobbyShop).Field<Teleport>("teleport").Value;
-            teleport.SetTeleportationEnabled(enabled: false);
-            Traverse.Create<BowserTriggerHandler>().Method("StopHobbyShopAmbience").GetValue();
-
-            // Originally from `GroupLaunchTable.OnStartButtonPressed`.
-            var playersInParty = Traverse.Create(groupLaunchTable).Field<int[]>("playersInParty").Value;
-            var groupLaunchTableData = Traverse.Create(groupLaunchTable).Field<GroupLaunchTableData>("data").Value;
-            groupLaunchTableData.photonView.RPC("BowserStartGroupLaunchRPC", RpcTarget.All, playersInParty[0], playersInParty[1], playersInParty[2], playersInParty[3], groupId, (int)moduleType, -1);
-
-            PhotonNetwork.SendAllOutgoingCommands();
-            BowserIntegration.ExitBowser();
-        }
-
-        private static void GroupLaunchTable_OnStartGroupLaunchRPC_Prefix()
+        private static bool GroupLaunchTable_OnStartGroupLaunchRPC_Prefix(GroupLaunchTable __instance, string groupId, int selectedModuleType, int randomIndex)
         {
             StopWatch.Restart();
+
+            if (HR.SelectedRuleset == Ruleset.None)
+            {
+                return true;
+            }
+
+            if (_hasJoinedTheRace)
+            {
+                return false;
+            }
+
+            HR.Logger.Msg("[HangoutGameRacer] Attempting to race out of Hangouts as table non-host.");
+            _hasJoinedTheRace = true;
+
+            var moduleType = (GroupLaunchModuleData.ModuleType)selectedModuleType;
+            if (moduleType == GroupLaunchModuleData.ModuleType.Random)
+            {
+                moduleType = (GroupLaunchModuleData.ModuleType)randomIndex;
+            }
+
+            RaceOutOfHangouts(__instance, groupId, moduleType, isTableHost: false);
+            return false;
         }
 
         private static bool SocialProviderParea_Constructor_Prefix(SocialProviderParea __instance, Action<ISocialProvider, JoinParameters> onJoinReceived)
         {
-            if (!_isStartingHangoutsGame)
+            if (!_hasJoinedTheRace)
             {
                 return true;
             }
@@ -121,11 +125,74 @@
 
         private static void CreatingGameState_OnJoinedRoom_Prefix()
         {
-            _isStartingHangoutsGame = false;
+            _hasJoinedTheRace = false;
+
+            if (!StopWatch.IsRunning)
+            {
+                return;
+            }
 
             StopWatch.Stop();
             var timeElapsed = StopWatch.Elapsed;
             HR.Logger.Msg($"[HangoutGameRacer] Time to join game from Hangouts: {timeElapsed.Seconds:00}.{timeElapsed.Milliseconds:00}s");
+        }
+
+        private static IEnumerable<CodeInstruction> PlayWithFriendsController_TryCreateTheRoom_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Ldc_I4 && instruction.operand.ToString().Contains("500"))
+                {
+                    yield return new CodeInstruction(OpCodes.Ldc_I4, 50);
+                    continue;
+                }
+
+                yield return instruction;
+            }
+        }
+
+        private static void RaceOutOfHangouts(GroupLaunchTable groupLaunchTable, string groupId, GroupLaunchModuleData.ModuleType moduleType, bool isTableHost)
+        {
+            var destination = ConvertModuleTypeToDestination(moduleType);
+            var stringToRoomCode = new StringToRoomCode(_coroutineSpinner);
+            stringToRoomCode.GetRoomCodeFromString(groupId, delegate(string roomCode)
+            {
+                _joinParameters = new JoinParameters
+                {
+                    groupId = GetPareaRoomCode(roomCode),
+                    gameId = string.Empty,
+                    destination = destination,
+                };
+
+                HR.Logger.Msg("[HangoutGameRacer] Pre-fetched room code.");
+                PrepareToLeaveHangouts(groupLaunchTable);
+
+                if (isTableHost)
+                {
+                    // Originally from `GroupLaunchTable.OnStartButtonPressed`.
+                    var playersInParty = Traverse.Create(groupLaunchTable).Field<int[]>("playersInParty").Value;
+                    var groupLaunchTableData = Traverse.Create(groupLaunchTable).Field<GroupLaunchTableData>("data").Value;
+                    groupLaunchTableData.photonView.RPC("BowserStartGroupLaunchRPC", RpcTarget.All, playersInParty[0], playersInParty[1], playersInParty[2], playersInParty[3], groupId, (int)moduleType, -1);
+                }
+
+                LeaveHangouts();
+            });
+        }
+
+        private static void PrepareToLeaveHangouts(GroupLaunchTable groupLaunchTable)
+        {
+            Traverse.Create(groupLaunchTable).Field<bool>("leavingBowser").Value = true;
+
+            // Recreating `GameStateHobbyShop.ExitBowser`
+            var teleport = Traverse.Create(_gameStateHobbyShop).Field<Teleport>("teleport").Value;
+            teleport.SetTeleportationEnabled(enabled: false);
+            Traverse.Create<BowserTriggerHandler>().Method("StopHobbyShopAmbience").GetValue();
+        }
+
+        private static void LeaveHangouts()
+        {
+            PhotonNetwork.SendAllOutgoingCommands();
+            BowserIntegration.ExitBowser();
         }
 
         private static GroupLaunchModuleData.ModuleType FindSelectedModelType(GroupLaunchTable groupLaunchTable)
