61fece734598dfd9fee2b87684931abc019933df
Edgar Orendain <edgar@orendainx.com>
2022-04-03 18:38:01 -0700

    Extend LevelSequence rule to allow arbitrarily long sequences.

--- HouseRules_Essentials/Rules/LevelSequenceOverriddenRule.cs
+++ HouseRules_Essentials/Rules/LevelSequenceOverriddenRule.cs
@@ -11,11 +11,9 @@
     using HouseRules.Types;
     using UnityEngine;
 
-    public sealed class LevelSequenceOverriddenRule : Rule, IConfigWritable<List<string>>, IMultiplayerSafe
+    public sealed class LevelSequenceOverriddenRule : Rule, IConfigWritable<List<string>>, IPatchable, IMultiplayerSafe
     {
         public override string Description => "LevelSequence is overridden";
-
-        protected override SyncableTrigger ModifiedSyncables => SyncableTrigger.NewPieceModified | SyncableTrigger.StatusEffectDataModified;
 
         private readonly List<string> _adjustments;
         private List<string> _originals;
@@ -42,10 +40,36 @@
             ReplaceExistingProperties(_originals);
         }
 
+        private static void Patch(Harmony harmony)
+        {
+            harmony.Patch(
+                original: AccessTools.Method(
+                    typeof(LevelSequenceConfiguration), "GetSequenceDefinition"),
+                prefix: new HarmonyMethod(typeof(LevelSequenceOverriddenRule), nameof(LevelSequenceConfiguration_GetSequenceDefinition_Prefix)));
+        }
+
+        private static bool LevelSequenceConfiguration_GetSequenceDefinition_Prefix(ref SequenceDefinition __result, int index, LevelSequence.GameType gameType)
+        {
+            var gameContext = Traverse.Create(typeof(GameHub)).Field<GameContext>("gameContext").Value;
+
+            var gameTypeSequence = gameContext.levelSequenceConfiguration.sequenceDefinitions.GetSequenceFromId(gameType, out _);
+            if (index >= 0 && index < gameTypeSequence.Length)
+            {
+                return true;
+            }
+
+            var indexToReturn = gameContext.levelManager.GetLevelSequence().CurrentLevelIsLastLevel
+                ? gameTypeSequence.Length - 1
+                : gameTypeSequence.Length - 3;
+
+            __result = gameTypeSequence[indexToReturn];
+            return false;
+        }
+
         /// <summary>
         /// Replaces LevelSequence levels with predefined list.
         /// </summary>
-        /// <returns>List of previous LevelSequnece levels that are now replaced.</returns>
+        /// <returns>List of previous LevelSequence levels that are now replaced.</returns>
         private static List<string> ReplaceExistingProperties(List<string> replacements)
         {
             var levelManager = Resources.FindObjectsOfTypeAll<LevelManager>().First();
