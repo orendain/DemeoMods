0f440643f773244f515866c2ce63948d576b8c34
Edgar Orendain <edgar@orendainx.com>
2022-04-18 02:13:02 -0700

    Update HR version to 1.4.0 for development.

--- HouseRules_Core/BuildVersion.cs
+++ HouseRules_Core/BuildVersion.cs
@@ -2,7 +2,7 @@
 {
     public static class BuildVersion
     {
-        public const string Version = "1.3.0";
-        public const string MajorMinorVersion = "1.3";
+        public const string Version = "1.4.0";
+        public const string MajorMinorVersion = "1.4";
     }
 }
