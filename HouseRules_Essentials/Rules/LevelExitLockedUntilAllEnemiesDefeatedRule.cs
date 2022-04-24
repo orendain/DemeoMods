e3ff895baec0368199de7beca208af8f7dd68518
Edgar Orendain <edgar@orendainx.com>
2022-04-24 06:09:53 -0700

    Fix the door staying locked for players when using the `LevelExitLockedUntilAllEnemiesDefeated` rule.

--- HouseRules_Essentials/Rules/LevelExitLockedUntilAllEnemiesDefeatedRule.cs
+++ HouseRules_Essentials/Rules/LevelExitLockedUntilAllEnemiesDefeatedRule.cs
@@ -101,6 +101,7 @@
 
             var levelExit = context.pieceAndTurnController.FindFirstPiece(p => p.HasPieceType(PieceType.LevelExit));
             levelExit?.DisableEffectState(EffectStateType.Locked);
+            HR.ScheduleResync();
         }
 
         private static bool IsEnemyRemaining(GameContext gameContext)
