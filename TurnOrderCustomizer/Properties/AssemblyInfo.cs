1366832c43b3f5c28f3d79a66af94b064d29bc6b
Edgar Orendain <edgar@orendainx.com>
2022-05-15 02:30:10 -0700

    Introduce a new mod: Turn Order Customizer

---
+++ TurnOrderCustomizer/Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using TurnOrderCustomizer;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Turn Order Customizer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Orendain")]
+[assembly: AssemblyProduct("Turn Order Customizer")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("17850BF6-EAFD-4F15-914D-CA660C58C73A")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+
+// Melon Loader.
+[assembly: MelonInfo(typeof(TurnOrderCustomizerMod), "Turn Order Customizer", "1.0.0", "Orendain", "https://github.com/orendain/DemeoMods")]
+[assembly: MelonGame("Resolution Games", "Demeo")]
+[assembly: MelonGame("Resolution Games", "Demeo PC Edition")]
+[assembly: MelonID("623605")]
+[assembly: VerifyLoaderVersion("0.5.3", true)]
---
+++ TurnOrderCustomizer/TurnOrderInjector.cs
@@ -0,0 +1,102 @@
+﻿namespace TurnOrderCustomizer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Boardgame.BoardEntities;
+    using Boardgame.TurnOrder;
+    using DataKeys;
+    using HarmonyLib;
+
+    internal static class TurnOrderInjector
+    {
+        internal static void Patch(Harmony harmony)
+        {
+            harmony.Patch(
+                original: AccessTools.Constructor(typeof(RearrangePlayerTurnOrder), new[] { typeof(TurnQueue) }),
+                prefix: new HarmonyMethod(typeof(TurnOrderInjector), nameof(RearrangePlayerTurnOrder_Constructor_Prefix)));
+        }
+
+        private static bool RearrangePlayerTurnOrder_Constructor_Prefix(
+            RearrangePlayerTurnOrder __instance,
+            TurnQueue turnQueue)
+        {
+            if (!TurnOrderCustomizerMod.PieceScorer.IsEnabled)
+            {
+                return true;
+            }
+
+            var playerPieces = turnQueue.GetPlayerPieces();
+            var playerScores = TurnOrderCustomizerMod.PieceScorer.ComputeScores(playerPieces);
+            var precomputedPlayerComparison = PrecomputedPieceComparison(playerScores);
+
+            playerPieces.Sort(precomputedPlayerComparison);
+
+            __instance.newTurnOrder = playerPieces.Select(piece => piece.networkID).ToArray();
+            Traverse.Create(__instance).Property<bool>("updateNeeded").Value = true;
+            return false;
+        }
+
+        /// <summary>
+        /// Returns a comparison that references the specified (pieceID, score) mappings.
+        /// </summary>
+        private static Comparison<Piece> PrecomputedPieceComparison(IReadOnlyDictionary<int, int> pieceScores)
+        {
+            return (piece1, piece2) =>
+            {
+                if (piece1 == piece2)
+                {
+                    return 0;
+                }
+
+                if (!pieceScores.TryGetValue(piece1.networkID, out var piece1Score))
+                {
+                    TurnOrderCustomizerMod.Logger.Warning(
+                        $"Could not find a turn order score for piece [{piece1.networkID}]. Resulting order may be unexpected.");
+                    return 1;
+                }
+
+                if (!pieceScores.TryGetValue(piece2.networkID, out var piece2Score))
+                {
+                    TurnOrderCustomizerMod.Logger.Warning(
+                        $"Could not find a turn order score for piece [{piece2.networkID}]. Resulting order may be unexpected.");
+                    return 1;
+                }
+
+                var scoreDifference = piece1Score - piece2Score;
+
+                // Return negative score difference as a high score represents priority in the turn order, whereas a comparison represents that with negative numbers.
+                return -scoreDifference;
+            };
+        }
+
+        /// <summary>
+        /// Returns a comparison representing the default Demeo piece comparison.
+        /// </summary>
+        /// <remarks>Unused, but included for reference.</remarks>
+        private static Comparison<Piece> DefaultPieceComparison()
+        {
+            return (piece1, piece2) =>
+            {
+                if (piece1 == piece2)
+                {
+                    return 0;
+                }
+
+                var score = (piece1.PlayerSpawnedIndex < piece2.PlayerSpawnedIndex) ? -1 : 1;
+
+                if (piece1.HasEffectState(EffectStateType.Downed))
+                {
+                    score += 2;
+                }
+
+                if (piece2.HasEffectState(EffectStateType.Downed))
+                {
+                    score -= 2;
+                }
+
+                return score;
+            };
+        }
+    }
+}
