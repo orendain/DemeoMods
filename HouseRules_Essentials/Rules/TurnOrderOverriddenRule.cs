cd3956ce0dd7080686b7c855f873ea8f1f99c470
Edgar Orendain <edgar@orendainx.com>
2022-05-17 00:31:16 -0700

    Log changes

--- HouseRules_Essentials/Rules/TurnOrderOverriddenRule.cs
+++ HouseRules_Essentials/Rules/TurnOrderOverriddenRule.cs
@@ -136,14 +136,14 @@
                 if (!pieceScores.TryGetValue(piece1.networkID, out var piece1Score))
                 {
                     EssentialsMod.Logger.Warning(
-                        $"[TurnOrderOverriddenRule] Could not find a turn order score for piece [{piece1.networkID}]. Resulting order may be unexpected.");
+                        $"[TurnOrderOverridden] Could not find a turn order score for piece [{piece1.networkID}]. Resulting order may be unexpected.");
                     return 1;
                 }
 
                 if (!pieceScores.TryGetValue(piece2.networkID, out var piece2Score))
                 {
                     EssentialsMod.Logger.Warning(
-                        $"[TurnOrderOverriddenRule] Could not find a turn order score for piece [{piece2.networkID}]. Resulting order may be unexpected.");
+                        $"[TurnOrderOverridden] Could not find a turn order score for piece [{piece2.networkID}]. Resulting order may be unexpected.");
                     return 1;
                 }
 
