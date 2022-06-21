f396641367b133bcd9f9f00a79ae737e30ac99b7
Edgar Orendain <edgar@orendainx.com>
2022-06-20 20:51:16 -0700

    Display version on RoomFinder UI. (#364)
    * Display version on RoomFinder UI.

---
+++ RoomFinder/BuildVersion.cs
@@ -0,0 +1,8 @@
+﻿namespace RoomFinder
+{
+    public static class BuildVersion
+    {
+        public const string Version = "1.5.0";
+        public const string MajorMinorVersion = "1.5";
+    }
+}
