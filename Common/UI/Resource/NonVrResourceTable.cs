ed1a7000f69d23ef3c0c696859f7a7c102f98e15
Edgar Orendain <edgar@orendainx.com>
2022-06-22 00:05:53 -0700

    Add NonVR utilities to the common module. (#372)
    * Refactor `common\ui` so as separate UI helpers based on UI environments.
    
    * Modify Common.csproj.
    
    * Modify HouseRules to account for the recent `common/ui` refactor.
    
    * Add NonVR utilities to the common module.
    
    Co-authored-by: Jim Conner <jimconner@users.noreply.github.com>

---
+++ Common/UI/Resource/NonVrResourceTable.cs
@@ -0,0 +1,76 @@
+﻿namespace Common.UI
+{
+    using System;
+    using System.Linq;
+    using UnityEngine;
+
+    internal class NonVrResourceTable
+    {
+        private static NonVrResourceTable _instance;
+
+        public Color ColorBrown { get; } = new Color(0.0392f, 0.0157f, 0, 1);
+
+        public Color ColorBeige { get; } = new Color(0.878f, 0.752f, 0.384f, 1);
+
+        public Sprite ButtonBlueNormal { get; private set; }
+
+        public Sprite ButtonGreenNormal { get; private set; }
+
+        public Sprite ButtonRedNormal { get; private set; }
+
+        public Sprite PaperDecorated { get; private set; }
+
+        public GameObject AnchorDesktopMainMenu { get; private set; }
+
+        public GameObject AnchorDesktopPages { get; private set; }
+
+        public GameObject AnchorNavigationBar { get; private set; }
+
+        public static NonVrResourceTable Instance()
+        {
+            if (_instance != null)
+            {
+                return _instance;
+            }
+
+            if (!IsReady())
+            {
+                throw new InvalidOperationException("NonVR UI resources not yet available.");
+            }
+
+            _instance = new NonVrResourceTable();
+            return _instance;
+        }
+
+        private NonVrResourceTable()
+        {
+            Refresh();
+        }
+
+        /// <summary>
+        /// Returns true if the all UI dependencies are met.
+        /// </summary>
+        internal static bool IsReady()
+        {
+            return Resources.FindObjectsOfTypeAll<Sprite>().Any(x => x.name == "ButtonMenuBlue")
+                   && Resources.FindObjectsOfTypeAll<Sprite>().Any(x => x.name == "ButtonMenuGreen")
+                   && Resources.FindObjectsOfTypeAll<Sprite>().Any(x => x.name == "ButtonMenuRed")
+                   && Resources.FindObjectsOfTypeAll<Sprite>().Any(x => x.name == "PaperDecorated")
+                   && Resources.FindObjectsOfTypeAll<GameObject>().Any(x => x.name == "DesktopMainMenu(Clone)")
+                   && Resources.FindObjectsOfTypeAll<GameObject>().Any(x => x.name == "Pages")
+                   && Resources.FindObjectsOfTypeAll<GameObject>().Any(x => x.name == "NavigationBar");
+        }
+
+        private void Refresh()
+        {
+            ButtonBlueNormal = Resources.FindObjectsOfTypeAll<Sprite>().First(x => x.name == "ButtonMenuBlue");
+            ButtonGreenNormal = Resources.FindObjectsOfTypeAll<Sprite>().First(x => x.name == "ButtonMenuGreen");
+            ButtonRedNormal = Resources.FindObjectsOfTypeAll<Sprite>().First(x => x.name == "ButtonMenuRed");
+            PaperDecorated = Resources.FindObjectsOfTypeAll<Sprite>().First(x => x.name == "PaperDecorated");
+            AnchorDesktopMainMenu = Resources.FindObjectsOfTypeAll<GameObject>()
+                .First(x => x.name == "DesktopMainMenu(Clone)");
+            AnchorDesktopPages = Resources.FindObjectsOfTypeAll<GameObject>().First(x => x.name == "Pages");
+            AnchorNavigationBar = Resources.FindObjectsOfTypeAll<GameObject>().First(x => x.name == "NavigationBar");
+        }
+    }
+}
