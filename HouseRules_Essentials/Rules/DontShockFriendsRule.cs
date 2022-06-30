65194f8f1b36172267b2347e01469e51dfb4df9c
Jim Conner <noreply@github.com>
2022-06-30 17:52:45 +0100

    WIP

--- HouseRules_Essentials/Rules/DontShockFriendsRule.cs
+++ HouseRules_Essentials/Rules/DontShockFriendsRule.cs
@@ -1,16 +1,15 @@
 ﻿namespace HouseRules.Essentials.Rules
 {
-    using System;
-    using System.Collections.Generic;
     using Boardgame;
     using Boardgame.BoardEntities;
     using Boardgame.BoardEntities.Abilities;
-    using Boardgame.Data;
     using Boardgame.GameplayEffects;
+    using Boardgame.SerializableEvents;
     using DataKeys;
     using HarmonyLib;
     using HouseRules.Types;
     using UnityEngine;
+    using System.IO;
 
     public sealed class DontShockFriendsRule : Rule, IConfigWritable<bool>, IPatchable,
         IMultiplayerSafe
@@ -21,6 +20,7 @@
         private static bool _isActivated;
 
         private readonly bool _adjustments;
+        private static GameContext _gameContext;
 
         public DontShockFriendsRule(bool adjustments)
         {
@@ -33,6 +33,7 @@
         {
             _globalAdjustments = _adjustments;
             _isActivated = true;
+            _gameContext = gameContext;
         }
 
         protected override void OnDeactivate(GameContext gameContext) => _isActivated = false;
@@ -45,6 +46,12 @@
                 prefix: new HarmonyMethod(
                     typeof(DontShockFriendsRule),
                     nameof(ProjectileHitSequence_OnStarted_Prefix)));
+
+            harmony.Patch(
+                original: AccessTools.Method(typeof(SerializableEventQueue), "ValidateSerializableEvent"),
+                prefix: new HarmonyMethod(
+                    typeof(DontShockFriendsRule),
+                    nameof(SerializableEventQueue_ValidateSerializableEvent_Postfix)));
         }
 
         private static bool ProjectileHitSequence_OnStarted_Prefix(ref ProjectileHitSequence __instance)
@@ -64,7 +71,30 @@
             {
                 var localizedText = Traverse.Create(damage).Method("GetLocalizedText", paramTypes: new[] { typeof(string), typeof(bool) }, arguments: new object[] { "Ui/pieceUi/notification/damage/noDamage", false }).GetValue<string>();
                 Notification.ShowGoldenText(new Target(targetPiece).gameObject, localizedText);
+                _gameContext.serializableEventQueue.SendEventRequest(new SerializeableEventSetStatusEffects(targetPiece.networkID, new EffectStateType[] { }, new EffectStateType[] { EffectStateType.Stunned }));
+
                 return false; // Don't run the original OnStarted method.
+            }
+
+            return true;
+        }
+
+        private static bool SerializableEventQueue_ValidateSerializableEvent_Postfix(byte[] serializableEventData, ref SerializableEvent __result)
+        {
+            if (!_isActivated)
+            {
+                return true;
+            }
+
+            if (__result == null)
+            {
+                SerializableEvent action = SerializableEvent.Deserialize(new BinaryReader(new MemoryStream(serializableEventData)));
+                if (action.type == SerializableEvent.Type.SetStatusEffects)
+                {
+                    EssentialsMod.Logger.Msg($"Bypassing Validate for {action.type}");
+                    __result = action;
+                    return false; // Don't run the original OnStarted method.
+                }
             }
 
             return true;
