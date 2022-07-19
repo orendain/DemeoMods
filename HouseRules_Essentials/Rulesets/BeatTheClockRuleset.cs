cd89eab0f1b5b0a27de0e0c2dc1df7a9e05dd0bd
The Gray Alien <badashwow@gmail.com>
2022-07-19 01:20:14 -0400

    Unnecessary - already in main
    n/a

--- HouseRules_Essentials/Rulesets/BeatTheClockRuleset.cs
+++ HouseRules_Essentials/Rulesets/BeatTheClockRuleset.cs
@@ -19,7 +19,6 @@
                 new PieceConfigAdjustedRule.PieceProperty { Piece = BoardPieceId.HeroHunter, Property = "StartHealth", Value = 200 },
                 new PieceConfigAdjustedRule.PieceProperty { Piece = BoardPieceId.HeroRogue, Property = "StartHealth", Value = 200 },
                 new PieceConfigAdjustedRule.PieceProperty { Piece = BoardPieceId.HeroSorcerer, Property = "StartHealth", Value = 200 },
-                new PieceConfigAdjustedRule.PieceProperty { Piece = BoardPieceId.HeroWarlock, Property = "StartHealth", Value = 200 },
             });
             var recyclingRule = new CardEnergyFromRecyclingMultipliedRule(5);
             var roundLimitRule = new RoundCountLimitedRule(15);
