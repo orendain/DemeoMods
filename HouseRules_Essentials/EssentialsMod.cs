d191dea3e452b4039e28ecc22006cfac6e657157
Jim Conner <noreply@github.com>
2022-07-27 22:01:23 +0100

    New Potion Commotion ruleset where all card additions are potions.

--- HouseRules_Essentials/EssentialsMod.cs
+++ HouseRules_Essentials/EssentialsMod.cs
@@ -79,6 +79,7 @@
             HR.Rulebook.Register(BetterSorcererRuleset.Create());
             HR.Rulebook.Register(NoSurprisesRuleset.Create());
             HR.Rulebook.Register(QuickAndDeadRuleset.Create());
+            HR.Rulebook.Register(PotionCommotionRuleset.Create());
         }
     }
 }
