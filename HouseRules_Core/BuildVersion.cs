dc01f0b750c078b8b3c8e5f19079666bd8b7ea21
Jim Conner <noreply@github.com>
2022-10-26 17:42:08 +0100

    Bump verison number for next release.

--- HouseRules_Core/BuildVersion.cs
+++ HouseRules_Core/BuildVersion.cs
@@ -2,7 +2,7 @@
 {
     public static class BuildVersion
     {
-        public const string Version = "1.4.0";
-        public const string MajorMinorVersion = "1.4";
+        public const string Version = "1.5.0";
+        public const string MajorMinorVersion = "1.5";
     }
 }
