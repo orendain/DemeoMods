9e87200fa7663a5c4a9e08d2e6adc14494731f2c
Jim Conner <jimconner@users.noreply.github.com>
2023-01-03 22:15:25 +0000

    Tidy-up

--- docs/scripts/settings_reference.sh
+++ docs/scripts/settings_reference.sh
@@ -10,7 +10,8 @@ if [ "$#" -eq 0 ]; then
 	# Environment variable is required if we're runnign without arguments.
 	if [ -z "${SOURCEDIR}" ]; then
 		echo "SOURCEDIR variable must be set if not using positional arguments"
-		echo "Usage: SOURCEDIR='./dnSpy Export v1.25/Assembly-CSharp' settings_reference.sh"
+		echo "Usage: SOURCEDIR=<ASSembly-CSharp_Dir_Path> ./scripts/settings_reference.sh"
+		echo "Example: SOURCEDIR='./dnSpy Export v1.25/Assembly-CSharp' settings_reference.sh"
 		echo "or   : ./settings_refrence.sh './dnSpy Export v1.25/Assembly-CSharp'"
 	        exit 1
 	fi
@@ -19,7 +20,6 @@ elif [ "$#" -eq 1 ]; then
 	SOURCEDIR="${1}"
 fi
 
-
 cat << 'EOF'
 # Demeo Parameter Names
 
@@ -27,12 +27,10 @@ A full list of all of the different parameter names used by HouseRules rules
 
 EOF
 
-
 for item in AbilityKey Behaviour BoardPieceId EffectStateType PieceType 
 do
 	echo "## ${item}(s)"
 	echo
-	#cat "${SOURCEDIR}/DataKeys/${item}.cs" | grep , | sed s/,// | sed s/^/-\ /
 	cat "${SOURCEDIR}/DataKeys/${item}.cs" | grep , | sort | awk '{print $1}' | sed 's/,//' | sed 's/^[[:space:]]*//' | sed 's/^/-\ /'
 
 done
