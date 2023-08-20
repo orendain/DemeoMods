24279bf2d585b4eb152df2f772aaee86b593cc37
The Gray Alien <badashwow@gmail.com>
2023-08-20 18:41:19 -0400

    Removed Elven Queen fight map from random
    Map was too small and had only 1 banish point.

--- HouseRules_Essentials/Rules/SmallLevelSequenceOverriddenRule.cs
+++ HouseRules_Essentials/Rules/SmallLevelSequenceOverriddenRule.cs
@@ -22,10 +22,10 @@
         private readonly List<string> _adjustments;
 
         private readonly List<string> elvenFloors1 = new List<string>
-                    { "ElvenFloor01", "ElvenFloor04", "ElvenFloor14", "ElvenFloor16", "ElvenFloor15" };
+                    { "ElvenFloor01", "ElvenFloor04", "ElvenFloor14", "ElvenFloor16" };
 
         private readonly List<string> elvenFloors2 = new List<string>
-                    { "ElvenFloor03", "ElvenFloor07", "ElvenFloor13", "ElvenFloor17", "ElvenFloor15", "ElvenFloor14" };
+                    { "ElvenFloor03", "ElvenFloor07", "ElvenFloor13", "ElvenFloor17", "ElvenFloor14" };
 
         private readonly List<string> forestFloors1 = new List<string>
                     { "ForestFloor01", "ForestFloor07" };
