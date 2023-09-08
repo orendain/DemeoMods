fa8663164ca582a07bde7b097b399261e99707b7
The Gray Alien <badashwow@gmail.com>
2023-09-08 18:37:02 -0400

    Moving on up...
    Also remove VigorPotion from RegainAbilityIfMaxxedOutOverriddenRule in Arachnophobia because it doesn't have a stat that can be checked.

--- HouseRules_Core/BuildVersion.cs
+++ HouseRules_Core/BuildVersion.cs
@@ -2,7 +2,7 @@
 {
     public static class BuildVersion
     {
-        public const string Version = "1.6.2";
-        public const string MajorMinorVersion = "1.6";
+        public const string Version = "1.7.0";
+        public const string MajorMinorVersion = "1.7";
     }
 }
