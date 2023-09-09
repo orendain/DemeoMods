23bc0fe651b79a47152bcc771b8346c60b273e8a
The Gray Alien <103865052+TheGrayAlien@users.noreply.github.com>
2023-09-09 04:41:52 -0400

    Update HouseRulesUiGameVr.cs

--- HouseRules_Configuration/UI/HouseRulesUiGameVr.cs
+++ HouseRules_Configuration/UI/HouseRulesUiGameVr.cs
@@ -52,7 +52,7 @@
             int numRules = 13;
             int textLength = 0;
             int returnCount = 0;
-            if (HR.SelectedRuleset.Longdesc != null && HR.SelectedRuleset.Longdesc != string.Empty)
+            if (HR.SelectedRuleset.Longdesc != string.IsNullOrEmpty())
             {
                 textLength = HR.SelectedRuleset.Longdesc.Length;
                 returnCount = HR.SelectedRuleset.Longdesc.Count(f => f == '\n');
