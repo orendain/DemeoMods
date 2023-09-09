3a960a7d3658280d1b12e4b0ef4abcf98dbd0e72
The Gray Alien <103865052+TheGrayAlien@users.noreply.github.com>
2023-09-09 06:05:59 -0400

    Update RoomFinder Build Version (#494)
    Update RoomFinder build version for release

--- RoomFinder/BuildVersion.cs
+++ RoomFinder/BuildVersion.cs
@@ -2,7 +2,7 @@
 {
     public static class BuildVersion
     {
-        public const string Version = "1.7.2";
-        public const string MajorMinorVersion = "1.7";
+        public const string Version = "1.8.0";
+        public const string MajorMinorVersion = "1.8";
     }
 }
