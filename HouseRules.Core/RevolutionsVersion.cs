721b2604d15665071add4346301ea4d7b80561b1
The Gray Alien <badashwow@gmail.com>
2023-09-20 16:20:22 -0400

    Forgot to update version...
    N/A

--- HouseRules.Core/RevolutionsVersion.cs
+++ HouseRules.Core/RevolutionsVersion.cs
@@ -2,7 +2,7 @@
 {
     public static class RevolutionsVersion
     {
-        public const string Version = "2.0.1";
+        public const string Version = "2.0.2";
         public const string MajorMinorVersion = "2.0";
     }
 }
