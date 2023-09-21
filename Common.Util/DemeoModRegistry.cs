accc1d65b3f648c7053c251fa7b3449f89e385b2
Edgar Orendain <edgar@orendainx.com>
2023-09-21 05:03:59 -0700

    dump

---
+++ Common.Util/DemeoModRegistry.cs
@@ -0,0 +1,52 @@
+﻿namespace Common.Util
+{
+    using System.Collections.Generic;
+    using Boardgame.Modding;
+    using HarmonyLib;
+
+    public static class DemeoModRegistry
+    {
+        /// <summary>
+        /// Registers a mod so that it appears in the in-game mod list.
+        /// </summary>
+        /// <param name="name">The name of the mod.</param>
+        /// <param name="version">The version of the mod.</param>
+        /// <param name="author">The author of the mod.</param>
+        /// <param name="description">The description of the mod.</param>
+        /// <param name="isNetworkCompatible">Whether the mod is network compatible.</param>
+        public static void RegisterMod(
+            string name,
+            string version,
+            string author,
+            string description,
+            bool isNetworkCompatible)
+        {
+            if (ModdingAPI.ExternallyInstalledMods == null)
+            {
+                ModdingAPI.ExternallyInstalledMods = new List<ModdingAPI.ModInformation>();
+            }
+
+            ModdingAPI.ExternallyInstalledMods.Add(new ModdingAPI.ModInformation
+            {
+                name = name,
+                version = version,
+                author = author,
+                description = description,
+                isNetworkCompatible = isNetworkCompatible,
+            });
+        }
+
+        /// <summary>
+        /// Unregisters a mod.
+        /// </summary>
+        /// <param name="name">The name of the mod.</param>
+        public static void UnregisterMod(string name)
+        {
+            ModdingAPI.ExternallyInstalledMods?.RemoveAll(m => m.name == name);
+            var installedMods = Traverse.Create(typeof(ModdingAPI)).
+                Field<List<ModdingAPI.ModInformation>>("installedMods").
+                Value;
+            installedMods?.RemoveAll(m => m.name == name);
+        }
+    }
+}
--- Highlighter/HighlighterMod.cs
+++ Highlighter/HighlighterMod.cs
@@ -1,15 +1,24 @@
 ﻿namespace Highlighter
 {
     using BepInEx;
+    using Common.Util;
     using HarmonyLib;
 
-    [BepInPlugin("com.orendain.demeomods.highlighter", "Highlighter", "2.0.0")]
+    [BepInPlugin(ModId, ModName, ModVersion)]
     public class HighlighterMod : BaseUnityPlugin
     {
+        private const string ModId = "com.orendain.demeomods.highlighter";
+        private const string ModName = "Highlighter";
+        private const string ModVersion = PluginInfo.PLUGIN_VERSION;
+        private const string ModAuthor = "DemeoMods Team";
+        private const string ModDescription = "In-game highlighting for gameplay tips.";
+        private const bool IsModNetworkCompatible = true;
+
         private void Awake()
         {
             var harmony = new Harmony("com.orendain.demeomods.highlighter");
             MoveHighlighter.Patch(harmony);
+            DemeoModRegistry.RegisterMod(ModName, ModVersion, ModAuthor, ModDescription, IsModNetworkCompatible);
         }
     }
 }
