f6588e5ea994b79d3ccea954dc18a8dd73574828
Edgar Orendain <edgar@orendainx.com>
2023-09-23 19:07:32 -0700

    wip

---
+++ HouseRules.Essentials/MelonLoaderMod.cs
@@ -0,0 +1,23 @@
+﻿#if MELONLOADER
+using HouseRules.Essentials;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(MelonLoaderMod), HouseRulesEssentialsCore.ModName, HouseRulesEssentialsCore.ModVersion, HouseRulesEssentialsCore.ModAuthor, "https://github.com/orendain/DemeoMods")]
+[assembly: MelonGame("Resolution Games", "Demeo")]
+[assembly: MelonGame("Resolution Games", "Demeo PC Edition")]
+[assembly: MelonID("574512")]
+[assembly: VerifyLoaderVersion("0.5.3", true)]
+
+namespace HouseRules.Essentials
+{
+    internal class MelonLoaderMod : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            HouseRulesEssentialsCore.Init(this);
+            HouseRulesEssentialsCore.RegisterRuleTypes();
+            HouseRulesEssentialsCore.RegisterRulesets();
+        }
+    }
+}
+#endif
