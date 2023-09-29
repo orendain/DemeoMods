988b37ae207ee18baa8baa84c58d457b0b67bc63
Edgar Orendain <edgar@orendainx.com>
2023-09-28 20:47:00 -0700

    Introduce BepInEx 5.x as a compilation target for AdvancedStats mod. (#510)
    Please see https://github.com/orendain/DemeoMods/pull/506 for an
    equivalent description of this change.

---
+++ AdvancedStats/MelonLoaderMod.cs
@@ -0,0 +1,25 @@
+﻿#if MELONLOADER
+using AdvancedStats;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(MelonLoaderMod), AdvancedStatsBase.ModName, AdvancedStatsBase.ModVersion, AdvancedStatsBase.ModAuthor, "https://github.com/orendain/DemeoMods")]
+[assembly: MelonGame("Resolution Games", "Demeo")]
+[assembly: MelonGame("Resolution Games", "Demeo PC Edition")]
+[assembly: VerifyLoaderVersion("0.5.7", true)]
+
+namespace AdvancedStats
+{
+    internal class MelonLoaderMod : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            AdvancedStatsBase.Init(this);
+        }
+
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            AdvancedStatsBase.OnSceneLoaded(sceneName);
+        }
+    }
+}
+#endif
