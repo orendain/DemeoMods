5910865ddc27b28a37a1ebfa00a848a962035539
The Gray Alien <badashwow@gmail.com>
2024-01-14 15:33:35 -0500

    Comments for future updates
    N/A

--- HouseRules.Essentials/HouseRulesEssentialsBase.cs
+++ HouseRules.Essentials/HouseRulesEssentialsBase.cs
@@ -91,6 +91,7 @@
             HR.Rulebook.Register(typeof(FreeReplenishablesOnCritRule));
             HR.Rulebook.Register(typeof(GoldPickedUpMultipliedRule));
             HR.Rulebook.Register(typeof(GrappleUnhookedRule));
+            HR.Rulebook.Register(typeof(HeroesMonsterDeckAdditionRule));
             HR.Rulebook.Register(typeof(HeroesPieceProgressLostRule));
             HR.Rulebook.Register(typeof(HeroesPieceProgressRule));
             HR.Rulebook.Register(typeof(HeroesRule));
@@ -152,6 +153,8 @@
             HR.Rulebook.Register(FriendlyCompetition.Create());
             HR.Rulebook.Register(DarkestDankestDemeoRuleset.Create());
             HR.Rulebook.Register(Heroes_of_the_Voice_Machine.Create());
+            // HR.Rulebook.Register(Heroes_of_the_Voice_Machine_Easy.Create());
+            // HR.Rulebook.Register(Heroes_of_the_Voice_Machine_Insane.Create());
             HR.Rulebook.Register(HardcoreRuleset.Create());
             HR.Rulebook.Register(Arachnophobia.Create());
             HR.Rulebook.Register(Blobophobia.Create());
