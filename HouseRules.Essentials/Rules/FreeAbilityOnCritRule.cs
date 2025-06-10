937f1797ccd100ca50b19d39f6f820d3646d2c39
Edgar Orendain <edgar@orendainx.com>
2025-06-10 02:40:07 -0700

    Update rule `FreeAbilityOnCritRule`.

--- HouseRules.Essentials/Rules/FreeAbilityOnCritRule.cs
+++ HouseRules.Essentials/Rules/FreeAbilityOnCritRule.cs
@@ -13,6 +13,7 @@
     {
         public override string Description => "Hero critical hits give a free card";
 
+        private static Context _context;
         private static Dictionary<BoardPieceId, AbilityKey> _globalAdjustments;
         private static bool _isActivated;
 
@@ -27,6 +28,7 @@
 
         protected override void OnActivate(Context context)
         {
+            _context = context;
             _globalAdjustments = _adjustments;
             _isActivated = true;
         }
@@ -59,13 +61,17 @@
                 return;
             }
 
-            if (!_globalAdjustments.ContainsKey(source.boardPieceId))
+            if (!_globalAdjustments.TryGetValue(source.boardPieceId, out var abilityKey))
             {
                 return;
             }
 
-            source.TryAddAbilityToInventory(_globalAdjustments[source.boardPieceId], isReplenishable: false);
-            HR.ScheduleBoardSync();
+            var abilityPromise = _context.AbilityFactory.LoadAbility(abilityKey);
+            abilityPromise.OnLoaded(ability =>
+            {
+                source.TryAddAbilityToInventory(ability, isReplenishable: false);
+                HR.ScheduleBoardSync();
+            });
         }
     }
 }
